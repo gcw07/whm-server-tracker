e7f15ca39d01a5ad091959d20c85c9c42eec5266
Grant Williams <grant@gwscripts.com>
2017-08-01 00:21:27 -0500

    Remove default auth routes and use just the ones I need. This removes the registration routes.

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 // Server Routes...
 Route::group(['middleware' => 'auth', 'prefix' => 'servers'], function () {
