da24e3f449f10975d8023f834156d6b37398ece7
Grant Williams <grant@gwscripts.com>
2017-08-01 00:05:34 -0500

    Add common testing macros and a sign in helper function to base test case class.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,40 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\TestResponse;
+use PHPUnit\Framework\Assert;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        TestResponse::macro('data', function ($key) {
+            return $this->original->getData()[$key];
+        });
+
+        TestResponse::macro('assertViewIs', function ($name) {
+            Assert::assertEquals($name, $this->original->name());
+        });
+
+        TestResponse::macro('assertJsonHasErrors', function ($keys = []) {
+            Assert::assertArrayHasKey('errors', $this->json());
+
+            foreach (array_wrap($keys) as $key) {
+                Assert::assertArrayHasKey($key, $this->json()['errors']);
+            }
+        });
+    }
+
+    protected function signIn($user = null)
+    {
+        $user = $user ?: create('App\User');
+
+        $this->actingAs($user);
+
+        return $this;
+    }
 }
