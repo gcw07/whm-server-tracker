de26de51b230f480b39632d18ba87cb72a4f644f
Grant Williams <grant@gwscripts.com>
2017-08-02 23:39:04 -0500

    Add account model, controller, factory and migration.

---
+++ database/migrations/2017_08_03_040002_create_accounts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('server_id');
+            $table->string('domain');
+            $table->string('user');
+            $table->boolean('backup');
+            $table->boolean('suspended');
+            $table->string('suspend_reason');
+            $table->timestamp('suspend_time')->nullable();
+            $table->timestamp('setup_date');
+            $table->string('disk_used'); // in MB
+            $table->string('disk_limit'); // in MB
+            $table->string('plan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
