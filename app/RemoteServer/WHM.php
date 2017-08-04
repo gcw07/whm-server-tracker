8da84f0716b5b957defe725649e07f496245a570
Grant Williams <grant@gwscripts.com>
2017-08-04 16:59:32 -0500

    First implementation of WHM server api.

---
+++ app/RemoteServer/WHM.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\RemoteServer;
+
+use App\Exceptions\InvalidServerTypeException;
+use App\Exceptions\MissingTokenException;
+use Zttp\Zttp;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+
+class WHM
+{
+    protected $baseUrl;
+    protected $header;
+    protected $server;
+
+    public function __construct($server)
+    {
+        $this->server = $server;
+
+        if ($this->server->server_type === 'reseller') {
+            throw new InvalidServerTypeException;
+        }
+
+        if (! $this->server->token) {
+            throw new MissingTokenException;
+        }
+
+        $this->setupConnection();
+    }
+
+    public static function create($server)
+    {
+        return new static($server);
+    }
+
+    public function getDiskUsage()
+    {
+        $url = "{$this->baseUrl}/getdiskusage?api.version=1";
+    }
+
+    private function setupConnection()
+    {
+        $config = config('server-tracker');
+
+        $host = "{$this->server->address}:{$this->server->port}";
+
+        $this->baseUrl = "{$config['urls']['protocol']}://{$host}/{$config['urls']['api-path']}";
+
+        $this->header = "Authorization: whm {$config['remote']['username']}:{$this->server->token}";
+
+//        $this->header = [
+//            'Authorization' => "whm {$config['remote']['username']}:{$this->server->token}"
+//        ];
+
+    }
+}
