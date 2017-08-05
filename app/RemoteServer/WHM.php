2849c248917406ae588e9d5e50ea382c3f5c1c92
Grant Williams <grant@gwscripts.com>
2017-08-04 22:12:47 -0500

    Clean up WHM api.

--- app/RemoteServer/WHM.php
+++ app/RemoteServer/WHM.php
@@ -4,14 +4,12 @@ namespace App\RemoteServer;
 
 use App\Exceptions\InvalidServerTypeException;
 use App\Exceptions\MissingTokenException;
-use Zttp\Zttp;
 use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Request;
 
 class WHM
 {
+    protected $authHeader;
     protected $baseUrl;
-    protected $header;
     protected $server;
 
     public function __construct($server)
@@ -38,34 +36,13 @@ class WHM
     {
         $url = "{$this->baseUrl}/getdiskusage?api.version=1";
 
-        /////////////////////////////////////
-//        $response = Zttp::withHeaders([
-//            'Authorization' => "whm root:XAT5VTR67T5"
-//        ])->withoutVerifying()->get($url);
-//
-//        dd($response->json());
-
-        //////////////////////////
         $client = new Client();
         $response = $client->request('GET', $url, [
-            'headers' => [
-                'Authorization' => "whm root:XAT5VTR67T5",
-            ],
+            'headers' => $this->authHeader,
             'verify' => false
         ]);
-        $data = json_decode($response->getBody(), true);
-        dd($data);
 
-        ////////////////////////
-        $client = new Client();
-        $request = new Request('GET', $url, [
-            'Authorization' => "whm root:XAT5VTR67T5"
-        ]);
-        dd($request);
-        $response = $client->send($request, ['verify' => false]);
-        $data = json_decode($response->getBody(), true);
-        dd($data);
-        /////////////////////////
+        return json_decode($response->getBody(), true);
     }
 
     private function setupConnection()
@@ -76,11 +53,8 @@ class WHM
 
         $this->baseUrl = "{$config['urls']['protocol']}://{$host}/{$config['urls']['api-path']}";
 
-        $this->header = "Authorization: whm {$config['remote']['username']}:{$this->server->token}";
-
-//        $this->header = [
-//            'Authorization' => "whm {$config['remote']['username']}:{$this->server->token}"
-//        ];
-
+        $this->authHeader = [
+            'Authorization' => "whm {$config['remote']['username']}:{$this->server->token}"
+        ];
     }
 }
