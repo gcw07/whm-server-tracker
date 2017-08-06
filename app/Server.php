071e3bdb6223a901a21111beda2407f83d080bcc
Grant Williams <grant@gwscripts.com>
2017-08-05 22:38:21 -0500

    Add ability to fetch disk usage details from a server.

--- app/Server.php
+++ app/Server.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\RemoteServer\WHM;
 use Illuminate\Database\Eloquent\Model;
 
 class Server extends Model
@@ -18,5 +19,31 @@ class Server extends Model
     public function addAccount($account)
     {
         return $this->accounts()->create($account);
+    }
+
+    public function fetchDiskUsageDetails()
+    {
+        try {
+            $api = WHM::create($this);
+
+            $diskUsage = $api->getDiskUsage();
+
+            $this->update([
+                'disk_used' => $diskUsage['used'],
+                'disk_available' => $diskUsage['available'],
+                'disk_total' => $diskUsage['total'],
+                'disk_percentage' => $diskUsage['percentage'],
+            ]);
+        } catch (InvalidServerTypeException $e) {
+
+        } catch (MissingTokenException $e) {
+
+        } catch (ServerConnectionException $e) {
+
+        } catch (ForbiddenAccessException $e) {
+
+        }
+
+        return false;
     }
 }
