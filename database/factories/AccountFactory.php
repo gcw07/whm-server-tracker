b0019ee957f8e16964e8d00d5b7865f48d555afb
Grant Williams <grant@gwscripts.com>
2017-08-09 10:21:48 -0500

    Make account factory a little more accurate with suspended information.

--- database/factories/AccountFactory.php
+++ database/factories/AccountFactory.php
@@ -1,10 +1,12 @@
 <?php
 
+use Carbon\Carbon;
 use Faker\Generator as Faker;
 
 $factory->define(App\Account::class, function (Faker $faker) {
     $domain = $faker->domainName;
     $user = explode('.', $domain)[0];
+    $suspended = $faker->boolean(10);
 
     return [
         'server_id' => function () {
@@ -14,9 +16,9 @@ $factory->define(App\Account::class, function (Faker $faker) {
         'user' => $user,
         'ip' => $faker->ipv4,
         'backup' => $faker->boolean(90),
-        'suspended' => $faker->boolean(5),
-        'suspend_reason' => 'not suspended',
-        'suspend_time' => null,
+        'suspended' => $suspended,
+        'suspend_reason' => $suspended ? 'account unpaid' : 'not suspended',
+        'suspend_time' => $suspended ? Carbon::now()->subDays(15) : null,
         'setup_date' => $faker->dateTimeBetween('-1 year', '-2 weeks'),
         'disk_used' => $faker->randomElement(['250M', '500M', '800M', '950M']),
         'disk_limit' => $faker->randomElement(['1000M', '3000M', '5000M', '10000M']),
