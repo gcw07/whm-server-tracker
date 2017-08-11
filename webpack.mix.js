9aea42bb4999554dd0cb4ea6364c6e93d41cd23b
Grant Williams <grant@gwscripts.com>
2017-08-11 04:42:04 -0500

    Add axios and lodash to extracted vendor files.

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-    .extract(['vue', 'buefy'])
+    .extract(['vue', 'buefy', 'axios', 'lodash'])
     .sass('resources/assets/sass/app.scss', 'public/css');
 
 if (mix.config.inProduction) {
