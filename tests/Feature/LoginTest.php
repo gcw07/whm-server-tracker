694871f68d3ab4b1c722c39479124c978f131b10
Grant Williams <grant@gwscripts.com>
2017-08-11 01:50:43 -0500

    Add tests for logging into site.

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function logging_in_with_valid_credentials()
+    {
+        $user = create('App\User', [
+            'email' => 'john@example.com',
+            'password' => bcrypt('super-secret-password'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => 'john@example.com',
+            'password' => 'super-secret-password',
+        ]);
+
+        $response->assertRedirect('/dashboard');
+
+        $this->assertTrue(Auth::check());
+        $this->assertTrue(Auth::user()->is($user));
+    }
+
+    /** @test */
+    function logging_in_with_invalid_credentials()
+    {
+        $user = create('App\User', [
+            'email' => 'john@example.com',
+            'password' => bcrypt('super-secret-password'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => 'john@example.com',
+            'password' => 'not-the-right-password',
+        ]);
+
+        $response->assertRedirect('/');
+
+        $response->assertSessionHasErrors('email');
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertFalse(Auth::check());
+    }
+
+    /** @test */
+    function logging_in_with_an_account_that_does_not_exist()
+    {
+        $response = $this->post('/login', [
+            'email' => 'nobody@example.com',
+            'password' => 'not-the-right-password',
+        ]);
+
+        $response->assertRedirect('/');
+
+        $response->assertSessionHasErrors('email');
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertFalse(Auth::check());
+    }
+
+    /** @test */
+    function logging_out_the_current_user()
+    {
+        $this->signIn();
+
+        $response = $this->post('/logout');
+
+        $response->assertRedirect('/');
+        $this->assertFalse(Auth::check());
+    }
+}
