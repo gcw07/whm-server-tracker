ea554dfbcae9c164abc569acb33d6eb09788b72c
Grant Williams <grant@gwscripts.com>
2017-08-11 02:31:55 -0500

    Remove bootstrap jquery library and comment out jquery for now. Hope to not use it.

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,17 +1,9 @@
 
 window._ = require('lodash');
 
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-try {
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
-} catch (e) {}
+// try {
+//     window.$ = window.jQuery = require('jquery');
+// } catch (e) {}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
