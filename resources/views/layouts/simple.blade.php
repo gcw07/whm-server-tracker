72d4b78103e9f865ad5ce40caaa7b5fe022a9119
Grant Williams <grant@gwscripts.com>
2017-08-11 04:37:35 -0500

    Start using javascript in the simple layout.

--- resources/views/layouts/simple.blade.php
+++ resources/views/layouts/simple.blade.php
@@ -32,5 +32,9 @@
         </div>
     </footer>
 
+    <!-- Scripts -->
+    <script src="{{ mix('js/manifest.js') }}"></script>
+    <script src="{{ mix('js/vendor.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
