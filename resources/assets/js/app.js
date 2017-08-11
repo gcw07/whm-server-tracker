6d069472d25642514c5c4e8fedd9dd2024db1ba7
Grant Williams <grant@gwscripts.com>
2017-08-11 02:43:50 -0500

    Import Vue, Buefy and use Buefy. Remove example component.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,21 +1,24 @@
 
 /**
  * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
+ * includes Vue and other libraries.
  */
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+// window.Vue = require('vue');
+
+import Vue from 'vue'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css
+
+Vue.use(Buefy)
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
-Vue.component('example', require('./components/Example.vue'));
 
 const app = new Vue({
     el: '#app'
