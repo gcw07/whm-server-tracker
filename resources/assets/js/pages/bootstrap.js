e6813b08cffcfe17444df86b58af899be5a47a4d
Grant Williams <grant@gwscripts.com>
2017-08-12 23:31:46 -0500

    Setup javascript pages and the bootstrap file for it. Also add the server listings page vue component.

---
+++ resources/assets/js/pages/bootstrap.js
@@ -0,0 +1,15 @@
+/*
+ |--------------------------------------------------------------------------
+ | Server Tracker Components
+ |--------------------------------------------------------------------------
+ |
+ | Here we will load the Server Tracker components which makes up the core
+ | client application.
+ */
+
+/**
+ * Server Components ...
+ */
+Vue.component('servers-listing', require('./servers/servers-listing.vue'));
+// Vue.component('bookkeeper-create-client', require('./clients/create-client.vue'));
+// Vue.component('bookkeeper-edit-client', require('./clients/edit-client.vue'));
