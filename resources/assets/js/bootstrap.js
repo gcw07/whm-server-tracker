120607200c95809f84b385224942669d8be54bf0
Grant Williams <grant@gwscripts.com>
2017-08-14 14:43:11 -0500

    Comment back out jquery.

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -5,9 +5,9 @@ window._ = require('lodash');
 window.moment = require('moment');
 window.Promise = require('promise');
 
-try {
-    window.$ = window.jQuery = require('jquery');
-} catch (e) {}
+// try {
+//     window.$ = window.jQuery = require('jquery');
+// } catch (e) {}
 
 /**
  * Vue is a modern JavaScript library for building interactive web interfaces
