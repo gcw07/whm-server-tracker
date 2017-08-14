efd3f86d8e9a66288b7086f8b861c16b64d9f7d6
Grant Williams <grant@gwscripts.com>
2017-08-14 13:47:43 -0500

    An updated version of the form error class. Use a real javascript class.

---
+++ resources/assets/js/forms2/errors.js
@@ -0,0 +1,82 @@
+export default class Errors {
+    /**
+     * Create a new Errors instance.
+     */
+    constructor() {
+        this.errors = {};
+    }
+
+    /**
+     * Determine if we have any errors.
+     *
+     * @returns {boolean}
+     */
+    hasErrors() {
+        return Object.keys(this.errors).length > 0;
+    }
+
+    /**
+     * Determine if an errors exists for the given field.
+     *
+     * @param {string} field
+     */
+    has(field) {
+        return this.errors.hasOwnProperty(field);
+    }
+
+    /**
+     * Get all of the raw errors for the collection.
+     *
+     * @returns {*}
+     */
+    all() {
+        return this.errors;
+    }
+
+    /**
+     * Get all of the errors for the collection in a flat array.
+     *
+     * @returns {Array}
+     */
+    flatten() {
+        return _.flattenDeep(_.toArray(this.errors));
+    }
+
+    /**
+     * Retrieve the error message for a field.
+     *
+     * @param {string} field
+     */
+    get(field) {
+        if (this.errors[field]) {
+            return this.errors[field][0];
+        }
+    }
+
+    /**
+     * Set the raw errors for the collection.
+     *
+     * @param {object} errors
+     */
+    set(errors) {
+        if (typeof errors === 'object') {
+            this.errors = errors;
+        } else {
+            this.errors = {'form': ['Something went wrong. Please try again or contact customer support.']};
+        }
+    }
+
+    /**
+     * Clear one or all error fields.
+     *
+     * @param {string|null} field
+     */
+    clear(field) {
+        if (field) {
+            delete this.errors[field];
+            return;
+        }
+
+        this.errors = {};
+    }
+}
