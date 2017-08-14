041ba0ed38de5bf1ba095ed30927a1a56c7ac45d
Grant Williams <grant@gwscripts.com>
2017-08-14 14:13:43 -0500

    Update form class to be a real class we can just import.

---
+++ resources/assets/js/forms2/form.js
@@ -0,0 +1,144 @@
+import Errors from './errors';
+
+export default class Form {
+    /**
+     * Create a new Form instance.
+     *
+     * @param {object} data
+     */
+    constructor(data) {
+        this.originalData = data;
+
+        for (let field in data) {
+            this[field] = data[field];
+        }
+
+        this.errors = new Errors();
+
+        this.busy = false;
+        this.successful = false;
+    }
+
+    /**
+     * Start processing the form.
+     */
+    startProcessing() {
+        this.errors.clear();
+        this.busy = true;
+        this.successful = false;
+    }
+
+    /**
+     * Finish processing the form.
+     */
+    finishProcessing() {
+        this.busy = false;
+        this.successful = true;
+    }
+
+    /**
+     * Reset the form fields.
+     */
+    reset() {
+        for (let field in this.originalData) {
+            this[field] = '';
+        }
+
+        this.errors.clear();
+        this.busy = false;
+        this.successful = false;
+    }
+
+    /**
+     * Fetch all relevant data for the form.
+     */
+    data() {
+        let data = {};
+
+        for (let property in this.originalData) {
+            data[property] = this[property];
+        }
+
+        return data;
+    }
+
+    /**
+     * Send a POST request to the given URL.
+     *
+     * @param {string} url
+     */
+    post(url) {
+        return this.submit('post', url);
+    }
+
+    /**
+     * Send a PUT request to the given URL.
+     *
+     * @param {string} url
+     */
+    put(url) {
+        return this.submit('put', url);
+    }
+
+    /**
+     * Send a PATCH request to the given URL.
+     *
+     * @param {string} url
+     */
+    patch(url) {
+        return this.submit('patch', url);
+    }
+
+    /**
+     * Send a DELETE request to the given URL.
+     *
+     * @param {string} url
+     */
+    delete(url) {
+        return this.submit('delete', url);
+    }
+
+    /**
+     * Submit the form.
+     *
+     * @param {string} requestType
+     * @param {string} url
+     */
+    submit(requestType, url) {
+        return new Promise((resolve, reject) => {
+            this.startProcessing();
+
+            axios[requestType](url, this.data())
+                .then(response => {
+                    this.onSuccess(response.data);
+
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    this.onFail(error.response.data);
+
+                    reject(error.response.data);
+                });
+        });
+    }
+
+    /**
+     * Handle a successful form submission.
+     *
+     * @param {object} data
+     */
+    onSuccess(data) {
+        this.reset();
+        this.finishProcessing();
+    }
+
+    /**
+     * Handle a failed form submission.
+     *
+     * @param {object} errors
+     */
+    onFail(errors) {
+        this.busy = false;
+        this.errors.set(errors);
+    }
+}
