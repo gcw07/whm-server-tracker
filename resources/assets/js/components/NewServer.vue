24f27c7f0c989bd3b705301112f9feafb3ccdd93
Grant Williams <grant@gwscripts.com>
2017-08-17 00:10:42 -0500

    Remove method from form tag. It isn't needed since we are just doing vue forms.

--- resources/assets/js/components/NewServer.vue
+++ resources/assets/js/components/NewServer.vue
@@ -1,5 +1,5 @@
 <template>
-    <form method="POST">
+    <form>
         <div class="modal-card">
             <header class="modal-card-head">
                 <p class="modal-card-title">New Server</p>
