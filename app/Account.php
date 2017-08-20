faafb3282914bb4d7a3e4c7a811ea3fdb8de416f
Grant Williams <grant@gwscripts.com>
2017-08-19 23:17:38 -0500

    Add disk usage, cpanel url and whm url attributes. Cast backup and suspend to boolean. Append the new attributes to the returned data. Cast suspend time and setup date to dates.

--- app/Account.php
+++ app/Account.php
@@ -7,9 +7,34 @@ use Illuminate\Database\Eloquent\Model;
 class Account extends Model
 {
     protected $guarded = [];
+    protected $casts = ['backup' => 'boolean', 'suspended' => 'boolean'];
+    protected $dates = ['suspend_time', 'setup_date'];
+    protected $appends = ['disk_usage', 'cpanel_url', 'whm_url'];
 
     public function server()
     {
         return $this->belongsTo(Server::class);
+    }
+
+    public function getDiskUsageAttribute()
+    {
+        $diskUsed = substr($this->disk_used, 0, -1);
+        $diskLimit = substr($this->disk_limit, 0, -1);
+
+        return round(($diskUsed / $diskLimit) * 100, 1) . '%';
+    }
+
+    public function getCpanelUrlAttribute()
+    {
+        return "https://{$this->domain}/cpanel";
+    }
+
+    public function getWhmUrlAttribute()
+    {
+        if ($this->server->port == 2087) {
+            return "https://{$this->server->address}:{$this->server->port}";
+        }
+
+        return "http://{$this->server->address}:{$this->server->port}";
     }
 }
