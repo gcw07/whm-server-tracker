69b61e3e7d874a00ba1016a7ac74ed2b20b4b648
Grant Williams <grant@gwscripts.com>
2017-08-19 23:17:54 -0500

    Add tests for the new account attributes.

--- tests/Unit/AccountTest.php
+++ tests/Unit/AccountTest.php
@@ -16,4 +16,31 @@ class AccountTest extends TestCase
 
         $this->assertInstanceOf('App\Server', $account->server);
     }
+
+    /** @test */
+    public function can_get_cpanel_external_url()
+    {
+        $account = make('App\Account', ['domain' => 'mydomain.com']);
+
+        $this->assertEquals('https://mydomain.com/cpanel', $account->cpanel_url);
+    }
+
+    /** @test */
+    public function can_get_whm_external_url()
+    {
+        $server = create('App\Server', ['address' => '1.1.1.1', 'port' => 2087]);
+        $account = make('App\Account', ['server_id' => $server->id, 'domain' => 'mydomain.com']);
+
+        $this->assertEquals('https://1.1.1.1:2087', $account->whm_url);
+    }
+
+    /** @test */
+    public function can_get_disk_usage()
+    {
+        $accountA = make('App\Account', ['disk_used' => '300M', 'disk_limit' => '2000M']);
+        $accountB = make('App\Account', ['disk_used' => '350M', 'disk_limit' => '2000M']);
+
+        $this->assertEquals('15%', $accountA->disk_usage);
+        $this->assertEquals('17.5%', $accountB->disk_usage);
+    }
 }
