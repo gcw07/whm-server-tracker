2930347a263488b389fbfc69bbf582bd652e76b8
Grant Williams <grant@gwscripts.com>
2017-08-19 21:23:31 -0500

    Rename account controller to accounts.

---
+++ app/Http/Controllers/AccountsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Account;
+use Illuminate\Http\Request;
+
+class AccountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('accounts.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Account  $account
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Account $account)
+    {
+        //
+    }
+}
