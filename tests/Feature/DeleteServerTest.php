00e572b0d1d167904018d7d709cb2498f55dccca
Grant Williams <grant@gwscripts.com>
2017-08-23 01:01:05 -0500

    Add test to make sure accounts are deleted when a server is deleted.

--- tests/Feature/DeleteServerTest.php
+++ tests/Feature/DeleteServerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Account;
 use App\Server;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -36,5 +37,32 @@ class DeleteServerTest extends TestCase
 
         $response->assertStatus(204);
         $this->assertEquals(0, Server::count());
+    }
+
+    /** @test */
+    public function accounts_are_deleted_when_a_server_is_deleted()
+    {
+        $this->signIn();
+
+        $server = create('App\Server', [
+            'name' => 'my-server-name',
+        ]);
+
+        $accounts = create('App\Account', [
+            'server_id' => $server->id,
+        ], 2);
+
+        $this->assertEquals(1, Server::count());
+
+        tap(Server::first(), function ($server) {
+            $this->assertCount(2, $server->accounts);
+        });
+
+        $response = $this->deleteJson("/servers/{$server->id}");
+
+        $response->assertStatus(204);
+
+        $this->assertEquals(0, Server::count());
+        $this->assertEquals(0, Account::count());
     }
 }
