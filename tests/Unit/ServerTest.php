b8da8bc64f93ab462340f482a81dfb1d64355f05
Grant Williams <grant@gwscripts.com>
2017-08-25 16:18:53 -0500

    Add new tests for saving settings. Redesigning how server settings are saved so I don't have to change the database every time a new field is added.

--- tests/Unit/ServerTest.php
+++ tests/Unit/ServerTest.php
@@ -28,6 +28,57 @@ class ServerTest extends TestCase
     }
 
     /** @test */
+    public function a_server_can_add_a_setting()
+    {
+        $this->server->setSetting('disk_used', 100000);
+        $this->server->setSetting('disk_available', 200000);
+
+        $this->assertCount(2, $this->server->settings);
+        $this->assertEquals('100000', $this->server->getSetting('disk_used'));
+    }
+
+    /** @test */
+    public function a_server_can_get_a_setting()
+    {
+        $this->server->setSetting('disk_available', 200000);
+
+        $this->assertEquals('200000', $this->server->getSetting('disk_available'));
+    }
+
+    /** @test */
+    public function a_server_can_update_a_setting()
+    {
+        $this->server->setSetting('disk_available', 200000);
+        $this->assertEquals('200000', $this->server->getSetting('disk_available'));
+
+        $this->server->setSetting('disk_available', 500000);
+        $this->assertEquals('500000', $this->server->getSetting('disk_available'));
+    }
+
+    /** @test */
+    public function a_server_can_remove_a_setting()
+    {
+        $this->server->setSetting('disk_available', 200000);
+        $this->assertCount(1, $this->server->settings);
+
+        $this->server->removeSetting('disk_available');
+        $this->assertCount(0, $this->server->settings);
+    }
+
+    /** @test */
+    public function a_server_can_remove_all_settings()
+    {
+        $this->server->setSetting('disk_used', 10000);
+        $this->server->setSetting('disk_available', 200000);
+
+        $this->assertCount(2, $this->server->settings);
+
+        $this->server->removeAllSettings();
+
+        $this->assertCount(0, $this->server->settings);
+    }
+
+    /** @test */
     public function a_server_can_add_an_account()
     {
         $this->server->addAccount([
