4712ff6a9de8f81773c7b23fe38487805cea2c72
Grant Williams <grant@gwscripts.com>
2017-08-26 16:51:06 -0500

    Add moment, promise to vendor js file.

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-    .extract(['vue', 'buefy', 'axios', 'lodash'])
+    .extract(['axios', 'buefy', 'lodash', 'moment', 'promise', 'vue'])
     .sass('resources/assets/sass/app.scss', 'public/css');
 
 if (mix.config.inProduction) {
