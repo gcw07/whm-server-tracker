b6bea11d6c2b85ce8a10c98c89d065a997dbb38a
Grant Williams <grant@gwscripts.com>
2017-08-27 03:07:19 -0500

    Remove lodash from vendor file.

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-    .extract(['axios', 'buefy', 'lodash', 'moment', 'promise', 'vue'])
+    .extract(['axios', 'buefy', 'moment', 'promise', 'vue'])
     .sass('resources/assets/sass/app.scss', 'public/css');
 
 if (mix.config.inProduction) {
