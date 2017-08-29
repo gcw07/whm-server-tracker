03448e0ace992b830d2b13d01d316d1eb519de88
Grant Williams <grant@gwscripts.com>
2017-08-29 13:46:18 -0500

    Use the same style for checking unique on adding a new user.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -28,7 +28,7 @@ class UsersController extends Controller
     {
         $data = $this->validate($request, [
             'name'     => ['required', 'string', 'max:191'],
-            'email'    => ['required', 'string', 'email', 'max:191', 'unique:users'],
+            'email'    => ['required', 'string', 'email', 'max:191', Rule::unique('users')],
             'password' => ['required', 'string', 'min:6', 'confirmed']
         ]);
 
