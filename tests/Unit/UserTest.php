fa99389d930056bf91507fac97bcda99028f1237
Grant Williams <grant@gwscripts.com>
2017-08-31 03:18:01 -0500

    Add user unit tests to make sure password and remember token are never returned with data.

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function the_password_should_not_be_included_in_returned_data()
+    {
+        $user = make('App\User', [
+            'name'     => 'John Doe',
+            'password' => 'secret'
+        ]);
+
+        $this->assertArrayNotHasKey('password', $user->toArray());
+    }
+
+    /** @test */
+    public function the_remember_token_should_not_be_included_in_returned_data()
+    {
+        $user = make('App\User', [
+            'name'           => 'John Doe',
+            'remember_token' => 'some-token'
+        ]);
+
+        $this->assertArrayNotHasKey('remember_token', $user->toArray());
+    }
+}
