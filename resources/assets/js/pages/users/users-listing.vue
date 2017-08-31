4c70c35e4f8c21c3733403bb965726f12d708f5a
Grant Williams <grant@gwscripts.com>
2017-08-31 04:10:00 -0500

    Update user listings page to add new user modal, along with ability to delete a user.

--- resources/assets/js/pages/users/users-listing.vue
+++ resources/assets/js/pages/users/users-listing.vue
@@ -12,10 +12,16 @@
 
                 <!-- Right side -->
                 <div class="level-right">
-                    <p class="level-item"></p>
-                    <p class="level-item"></p>
-                    <p class="level-item"></p>
-                    <p class="level-item"></p>
+                    <p class="level-item">
+                        <button class="button" @click="isNewUserModalActive = true">
+                            <span class="icon is-small">
+                                <i class="fa fa-plus"></i>
+                            </span>
+                            <span>
+                                New
+                            </span>
+                        </button>
+                    </p>
                 </div>
             </nav>
         </div>
@@ -44,33 +50,17 @@
                                         </span>
                                     </button>
 
-                                    <b-dropdown-item has-link>
-                                        <a :href="item.domain_url" target="_blank">
-                                            <span class="icon is-small">
-                                                <i class="fa fa-globe"></i>
-                                            </span>
-                                            <span>View Site</span>
-                                        </a>
+                                    <b-dropdown-item @click="menuAction('edit', item)">
+                                        <span class="icon is-small">
+                                            <i class="fa fa-pencil"></i>
+                                        </span>
+                                        <span>Edit</span>
                                     </b-dropdown-item>
-
-                                    <hr class="dropdown-divider">
-
-                                    <b-dropdown-item has-link>
-                                        <a :href="item.cpanel_url" target="_blank">
-                                            <span class="icon is-small">
-                                                <i class="fa fa-link"></i>
-                                            </span>
-                                            <span>Access cPanel</span>
-                                        </a>
-                                    </b-dropdown-item>
-
-                                    <b-dropdown-item has-link>
-                                        <a :href="item.whm_url" target="_blank">
-                                            <span class="icon is-small">
-                                                <i class="fa fa-link"></i>
-                                            </span>
-                                            <span>Access Server WHM</span>
-                                        </a>
+                                    <b-dropdown-item @click="menuAction('remove', item)">
+                                        <span class="icon is-small">
+                                            <i class="fa fa-trash"></i>
+                                        </span>
+                                        <span>Remove</span>
                                     </b-dropdown-item>
                                 </b-dropdown>
                             </p>
@@ -79,13 +69,26 @@
                 </tr>
             </tbody>
         </table>
+
+        <b-modal :active.sync="isNewUserModalActive" :canCancel="false" has-modal-card>
+            <new-user @added="addUser"></new-user>
+        </b-modal>
     </div>
 </template>
 <script>
+    import NewUser from '../../components/NewUser'
+    import Form from '../../forms/form';
+
     export default {
+        components: {
+            NewUser
+        },
+
         data() {
             return {
                 items: false,
+                isNewUserModalActive: false,
+                deleteForm: new Form({}),
             };
         },
 
@@ -97,7 +100,47 @@
             fetch() {
                 axios.get('/api/users')
                     .then(response => this.items = response.data);
-            }
+            },
+
+            addUser(item) {
+                this.items.push(item);
+            },
+
+            deleteUser(item) {
+                this.$dialog.confirm({
+                    message: 'Are you sure you want to <strong>remove</strong> this user? This action can not be undone.',
+                    title: item.name,
+                    confirmText: 'Remove User',
+                    type: 'is-danger',
+                    onConfirm: () => {
+                        this.deleteForm.delete(`/users/${item.id}`)
+                            .then(response => {
+                                this.items.splice(this.items.indexOf(item), 1);
+
+                                this.$toast.open({
+                                    message: 'User Removed Successfully',
+                                    type: 'is-success',
+                                    duration: 4000
+                                });
+                            });
+                    }
+                })
+            },
+
+            menuAction(action, item) {
+                switch (action) {
+                    case 'edit':
+                        window.location.href = `/users/${item.id}/edit`;
+                        break;
+
+                    case 'remove':
+                        this.deleteUser(item);
+                        break;
+
+                    default:
+                        break;
+                }
+            },
         }
 
     }
