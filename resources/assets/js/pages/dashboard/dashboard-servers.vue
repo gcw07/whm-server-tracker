bb04c0969030090abd952fdf6dbc01555d326201
Grant Williams <grant@gwscripts.com>
2017-09-05 03:56:00 -0500

    Get the dashboard servers vue component working.

--- resources/assets/js/pages/dashboard/dashboard-servers.vue
+++ resources/assets/js/pages/dashboard/dashboard-servers.vue
@@ -5,29 +5,46 @@
 
             <table class="table is-fullwidth is-narrow">
                 <thead>
-                <tr>
-                    <th>Type</th>
-                    <th>#</th>
-                </tr>
+                    <tr>
+                        <th>Type</th>
+                        <th>#</th>
+                    </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>Dedicated</td>
-                    <td>1</td>
-                </tr>
-                <tr>
-                    <td>Reseller</td>
-                    <td>5</td>
-                </tr>
-                <tr>
-                    <td>VPS</td>
-                    <td>3</td>
-                </tr>
+                    <tr>
+                        <td>Dedicated</td>
+                        <td>{{ servers.dedicated }}</td>
+                    </tr>
+                    <tr>
+                        <td>Reseller</td>
+                        <td>{{ servers.reseller }}</td>
+                    </tr>
+                    <tr>
+                        <td>VPS</td>
+                        <td>{{ servers.vps }}</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
     </div>
 </template>
 <script>
+    export default {
+        data() {
+            return {
+                servers: false
+            };
+        },
 
+        mounted() {
+            this.fetch();
+        },
+
+        methods: {
+            fetch() {
+                axios.get('/api/dashboard/servers')
+                    .then(response => this.servers = response.data);
+            }
+        }
+    }
 </script>
