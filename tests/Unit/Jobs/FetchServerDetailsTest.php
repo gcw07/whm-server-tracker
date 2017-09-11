413ed031547b3c7fe23dc803a2b26e8a548c5432
Grant Williams <grant@gwscripts.com>
2017-09-11 04:35:51 -0500

    Test the fetch server details queue job.

---
+++ tests/Unit/Jobs/FetchServerDetailsTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit\Jobs;
+
+use App\Connectors\FakeServerConnector;
+use App\Connectors\ServerConnector;
+use App\Jobs\FetchServerDetails;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class FetchServerDetailsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_fetches_server_details()
+    {
+        $server = create('App\Server', [
+            'name'        => 'my-server-name',
+            'address'     => '1.1.1.1',
+            'port'        => 1000,
+            'server_type' => 'vps',
+            'token'       => 'valid-server-api-token',
+        ]);
+
+        $fake = new FakeServerConnector;
+        $this->app->instance(ServerConnector::class, $fake);
+
+        FetchServerDetails::dispatch($server);
+
+        tap($server->fresh(), function ($server) {
+            $this->assertEquals('my-server-name', $server->name);
+
+            $this->assertNotNull($server->settings()->get('disk_used'));
+            $this->assertNotNull($server->settings()->get('disk_available'));
+            $this->assertNotNull($server->settings()->get('disk_total'));
+            $this->assertNotNull($server->settings()->get('disk_percentage'));
+            $this->assertNotNull($server->settings()->get('backup_enabled'));
+            $this->assertNotNull($server->settings()->get('backup_days'));
+            $this->assertNotNull($server->settings()->get('backup_retention'));
+            $this->assertNotNull($server->settings()->get('php_version'));
+        });
+    }
+}
