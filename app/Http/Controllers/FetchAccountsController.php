5adbc7e37fb85367fcf7013181db916584aba219
Grant Williams <grant@gwscripts.com>
2017-09-11 04:02:10 -0500

    Cleanup the FetchAccounts and FetchDetails controllers. Remove unused imports and unused constructors.

--- app/Http/Controllers/FetchAccountsController.php
+++ app/Http/Controllers/FetchAccountsController.php
@@ -2,23 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Connectors\ServerConnector;
-use App\Exceptions\Server\ForbiddenAccessException;
-use App\Exceptions\Server\InvalidServerTypeException;
-use App\Exceptions\Server\MissingTokenException;
-use App\Exceptions\Server\ServerConnectionException;
 use App\Jobs\FetchServerAccounts;
 use App\Server;
 
 class FetchAccountsController extends Controller
 {
-    private $serverConnector;
-
-    public function __construct(ServerConnector $serverConnector)
-    {
-        $this->serverConnector = $serverConnector;
-    }
-
     /**
      * Update the specified resource in storage.
      *
--- app/Http/Controllers/FetchDetailsController.php
+++ app/Http/Controllers/FetchDetailsController.php
@@ -2,24 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Connectors\ServerConnector;
-use App\Exceptions\Server\ForbiddenAccessException;
-use App\Exceptions\Server\InvalidServerTypeException;
-use App\Exceptions\Server\MissingTokenException;
-use App\Exceptions\Server\ServerConnectionException;
 use App\Jobs\FetchServerDetails;
 use App\Server;
-use Carbon\Carbon;
 
 class FetchDetailsController extends Controller
 {
-    private $serverConnector;
-
-    public function __construct(ServerConnector $serverConnector)
-    {
-        $this->serverConnector = $serverConnector;
-    }
-
     /**
      * Update the specified resource in storage.
      *
