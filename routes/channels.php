e2ad7246b8617457d401a808373671b20aba9995
Grant Williams <grant@gwscripts.com>
2017-09-16 03:22:49 -0500

    Checks to see if the user is logged into the site for the broadcast channel.

--- routes/channels.php
+++ routes/channels.php
@@ -11,6 +11,6 @@
 |
 */
 
-Broadcast::channel('App.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('server-update', function ($user) {
+    return Auth::check();
 });
