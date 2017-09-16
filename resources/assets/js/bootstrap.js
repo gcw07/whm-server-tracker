94c2821ea2b154e6d3d7bb6cd53dc6c10fcb105e
Grant Williams <grant@gwscripts.com>
2017-09-16 04:01:47 -0500

    Add pusher and echo to the javascript bootstrap file.

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -53,3 +53,20 @@ require('./utilities/directives');
  * Define the Vue filters.
  */
 require('./utilities/filters');
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
+ */
+
+import Echo from 'laravel-echo';
+
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: App.pusherKey,
+    cluster: App.pusherCluster,
+    encrypted: true
+});
