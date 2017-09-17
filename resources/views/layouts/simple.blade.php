eff40df2ebd1d559bf35637e30c2c94907b5cad5
Grant Williams <grant@gwscripts.com>
2017-09-17 03:19:27 -0500

    Add pusher information to login template so that errors are not thrown when starting echo.

--- resources/views/layouts/simple.blade.php
+++ resources/views/layouts/simple.blade.php
@@ -14,6 +14,13 @@
     <link rel="icon" href="data:;base64,iVBORwOKGO=">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
+    <!-- Scripts -->
+    <script>
+        window.App = {!! json_encode([
+            'pusherKey' => config('broadcasting.connections.pusher.key'),
+            'pusherCluster' => config('broadcasting.connections.pusher.options.cluster'),
+        ]) !!};
+    </script>
     <script defer src="{{ mix('js/fontawesome-solid.js') }}"></script>
     <script defer src="{{ mix('js/fontawesome.js') }}"></script>
 </head>
