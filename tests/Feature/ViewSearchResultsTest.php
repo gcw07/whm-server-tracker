8b18737a25ee10eb846c9652892053e20d8a5bcf
Grant Williams <grant@gwscripts.com>
2017-09-17 05:53:54 -0500

    Add tests for searching server notes and domain ip

--- tests/Feature/ViewSearchResultsTest.php
+++ tests/Feature/ViewSearchResultsTest.php
@@ -73,6 +73,33 @@ class ViewSearchResultsTest extends TestCase
     }
 
     /** @test */
+    public function the_server_notes_can_be_searched()
+    {
+        $this->signIn();
+
+        $server = create('App\Server', [
+            'name'        => 'My Test Server',
+            'address'     => '255.1.1.100',
+            'port'        => 1111,
+            'notes'       => 'see this note',
+        ]);
+
+        $serverB = create('App\Server', [
+            'name'        => 'No Results',
+            'address'     => '2.1.1.100',
+            'port'        => 2222,
+            'notes'       => 'do not see me',
+        ]);
+
+        $response = $this->get("/search?q=this");
+
+        $response->assertStatus(200);
+
+        $response->assertSee($server->name)
+            ->assertDontSee($serverB->name);
+    }
+
+    /** @test */
     public function the_account_domain_can_be_searched()
     {
         $this->signIn();
@@ -88,6 +115,29 @@ class ViewSearchResultsTest extends TestCase
         ]);
 
         $response = $this->get("/search?q=Test");
+
+        $response->assertStatus(200);
+
+        $response->assertSee($account->domain)
+            ->assertDontSee($accountB->domain);
+    }
+
+    /** @test */
+    public function the_account_ip_can_be_searched()
+    {
+        $this->signIn();
+
+        $account = create('App\Account', [
+            'domain' => 'mytestsite.com',
+            'ip'     => '255.1.1.100',
+        ]);
+
+        $accountB = create('App\Account', [
+            'domain' => 'never-see.com',
+            'ip'     => '192.1.1.100',
+        ]);
+
+        $response = $this->get("/search?q=255");
 
         $response->assertStatus(200);
 
