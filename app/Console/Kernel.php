1107cb026b8ceef054d330585ce9d83d5ccd81ba
Grant Williams <grant@gwscripts.com>
2017-09-18 06:16:26 -0500

    Add the scheduled commands, horizon snapshots every 5 minutes and server tracker refresh command every hour.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        // Schedule Horizon Snapshots
+        $schedule->command('horizon:snapshot')->everyFiveMinutes();
+
+        // Schedule Server Tracker
+        $schedule->command('server-tracker:refresh')->hourly();
     }
 
     /**
