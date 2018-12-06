8bb09f78389c471c300ec318775f28b6c0e77a13
Grant Williams <grant@gwscripts.com>
2018-12-06 04:18:42 -0600

    Allow admins to access websockets admin dashboard on live.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('viewWebSocketsDashboard', function ($user = null) {
+            return in_array($user->email, config('server-tracker.admin_emails'));
+        });
     }
 }
