97d939f8991ddac0a66470ccd764e6cb386a24c9
Grant Williams <grant@gwscripts.com>
2018-12-06 03:25:02 -0600

    Switch the front end from pusher over to laravel websockets.

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,6 +38,9 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => env('PUSHER_APP_ENCRYPTED'),
+                'host' => '127.0.0.1',
+                'port' => 6001,
+                'scheme' => 'http'
             ],
         ],
 
