c2f59bdb6faf502ab23587a79b91f58bdf2b1a94
Grant Williams <grant@gwscripts.com>
2018-12-06 04:16:59 -0600

    Upgrade to Laravel Horizon 2.0

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,6 @@ use App\Connectors\ServerConnector;
 use App\Connectors\WHMServerConnector;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Horizon\Horizon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,17 +27,5 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(ServerConnector::class, WHMServerConnector::class);
-
-        Horizon::auth(function ($request) {
-            if ($request->user()) {
-                $config = config('server-tracker');
-
-                if (in_array($request->user()->email, $config['horizon_admin_emails'])) {
-                    return true;
-                }
-            }
-
-            return false;
-        });
     }
 }
