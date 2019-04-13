f15c7471241a26bae706f21d6f9424b77a590e35
Grant Williams <grant@gwscripts.com>
2019-04-13 14:05:05 -0500

    Redefine routes to use tuple format instead of text @ method.

--- routes/web.php
+++ routes/web.php
@@ -1,69 +1,87 @@
 <?php
 
+use App\Http\Controllers\AccountsController;
+use App\Http\Controllers\Api\AccountsListingsController;
+use App\Http\Controllers\Api\DashboardLatestAccountsController;
+use App\Http\Controllers\Api\DashboardServersController;
+use App\Http\Controllers\Api\DashboardStatsController;
+use App\Http\Controllers\Api\ServersListingsController;
+use App\Http\Controllers\Api\UsersListingsController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FetchAccountsController;
+use App\Http\Controllers\FetchDetailsController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\ServersController;
+use App\Http\Controllers\ServersTokenController;
+use App\Http\Controllers\UsersChangePasswordController;
+use App\Http\Controllers\UsersController;
+
 Route::redirect('/', '/dashboard');
 
 // Authentication Routes...
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
 // Password Reset Routes...
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('password/reset', [ResetPasswordController::class, 'reset']);
 
 // Dashboard Routes...
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
 
 // Account Routes...
 Route::group(['middleware' => 'auth', 'prefix' => 'accounts'], function () {
-    Route::get('/', 'AccountsController@index')->name('accounts.index');
-    Route::get('/{server}', 'AccountsController@index')->name('accounts.server-index');
-//    Route::get('/{account}', 'AccountsController@show')->name('accounts.show');
+    Route::get('/', [AccountsController::class, 'index'])->name('accounts.index');
+    Route::get('/{server}', [AccountsController::class, 'index'])->name('accounts.server-index');
 });
 
 // Server Routes...
 Route::group(['middleware' => 'auth', 'prefix' => 'servers'], function () {
-    Route::post('/', 'ServersController@store')->name('servers.store');
-    Route::get('/', 'ServersController@index')->name('servers.index');
-    Route::delete('/{server}', 'ServersController@destroy')->name('servers.destroy');
-    Route::put('/{server}', 'ServersController@update')->name('servers.update');
-    Route::get('/{server}', 'ServersController@show')->name('servers.show');
-    Route::get('/{server}/edit', 'ServersController@edit')->name('servers.edit');
+    Route::post('/', [ServersController::class, 'store'])->name('servers.store');
+    Route::get('/', [ServersController::class, 'index'])->name('servers.index');
+    Route::delete('/{server}', [ServersController::class, 'destroy'])->name('servers.destroy');
+    Route::put('/{server}', [ServersController::class, 'update'])->name('servers.update');
+    Route::get('/{server}', [ServersController::class, 'show'])->name('servers.show');
+    Route::get('/{server}/edit', [ServersController::class, 'edit'])->name('servers.edit');
 
-    Route::put('/{server}/token', 'ServersTokenController@update')->name('servers.token');
-    Route::delete('/{server}/token', 'ServersTokenController@destroy')->name('servers.token-destroy');
+    Route::put('/{server}/token', [ServersTokenController::class, 'update'])->name('servers.token');
+    Route::delete('/{server}/token', [ServersTokenController::class, 'destroy'])->name('servers.token-destroy');
 
-    Route::get('/{server}/fetch-details', 'FetchDetailsController@update')->name('servers.fetch-details');
-    Route::get('/{server}/fetch-accounts', 'FetchAccountsController@update')->name('servers.fetch-accounts');
+    Route::get('/{server}/fetch-details', [FetchDetailsController::class, 'update'])->name('servers.fetch-details');
+    Route::get('/{server}/fetch-accounts', [FetchAccountsController::class, 'update'])->name('servers.fetch-accounts');
 });
 
 // User Routes...
 Route::group(['middleware' => 'auth', 'prefix' => 'users'], function () {
-    Route::post('/', 'UsersController@store')->name('users.store');
-    Route::get('/', 'UsersController@index')->name('users.index');
-    Route::delete('/{user}', 'UsersController@destroy')->name('users.destroy');
-    Route::put('/{user}', 'UsersController@update')->name('users.update');
-    Route::get('/{user}/edit', 'UsersController@edit')->name('users.edit');
+    Route::post('/', [UsersController::class, 'store'])->name('users.store');
+    Route::get('/', [UsersController::class, 'index'])->name('users.index');
+    Route::delete('/{user}', [UsersController::class, 'destroy'])->name('users.destroy');
+    Route::put('/{user}', [UsersController::class, 'update'])->name('users.update');
+    Route::get('/{user}/edit', [UsersController::class, 'edit'])->name('users.edit');
 
-    Route::put('/{user}/change-password', 'UsersChangePasswordController@update')->name('users.change-password');
+    Route::put('/{user}/change-password', [UsersChangePasswordController::class, 'update'])->name('users.change-password');
 });
 
 // Search Routes...
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('/search', 'SearchController@index')->name('search');
+    Route::get('/search', [SearchController::class, 'index'])->name('search');
 });
 
 // API Routes...
 Route::group(['middleware' => 'auth', 'prefix' => 'api'], function () {
-    Route::get('/accounts', 'Api\AccountsListingsController@index')->name('accounts.listing');
-    Route::get('/accounts/{server}', 'Api\AccountsListingsController@index')->name('accounts.server-listing');
-    Route::get('/dashboard/stats', 'Api\DashboardStatsController@index')->name('dashboard.stats');
-    Route::get('/dashboard/servers', 'Api\DashboardServersController@index')->name('dashboard.servers');
-    Route::get('/dashboard/latest-accounts', 'Api\DashboardLatestAccountsController@index')->name('dashboard.latest-accounts');
-    Route::get('/servers', 'Api\ServersListingsController@index')->name('servers.listing');
-    Route::get('/users', 'Api\UsersListingsController@index')->name('users.listing');
+    Route::get('/accounts', [AccountsListingsController::class, 'index'])->name('accounts.listing');
+    Route::get('/accounts/{server}', [AccountsListingsController::class, 'index'])->name('accounts.server-listing');
+    Route::get('/dashboard/stats', [DashboardStatsController::class, 'index'])->name('dashboard.stats');
+    Route::get('/dashboard/servers', [DashboardServersController::class, 'index'])->name('dashboard.servers');
+    Route::get('/dashboard/latest-accounts', [DashboardLatestAccountsController::class, 'index'])->name('dashboard.latest-accounts');
+    Route::get('/servers', [ServersListingsController::class, 'index'])->name('servers.listing');
+    Route::get('/users', [UsersListingsController::class, 'index'])->name('users.listing');
 });
