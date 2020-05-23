55cf4fb1fd8059eaf645c1e047c86801e6020eba
Grant Williams <grant@gwscripts.com>
2020-05-23 04:36:13 -0500

    Update the database factories and seeder.

--- database/factories/AccountFactory.php
+++ database/factories/AccountFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
 use Carbon\Carbon;
 use Faker\Generator as Faker;
 
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,8 +1,10 @@
 <?php
 
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
 use App\User;
-use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,12 +18,11 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(User::class, function (Faker $faker) {
-    static $password;
-
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'email_verified_at' => now(),
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
 });
