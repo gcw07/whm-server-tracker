3283f8092c408e950c268102f648a656cef4254d
Grant Williams <grant@gwscripts.com>
2020-05-23 02:58:00 -0500

    Update most of the configs for L7.

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => env('PUSHER_APP_ENCRYPTED'),
+                'useTLS' => true,
             ],
         ],
 
