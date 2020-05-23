d0030f737e93f364ef8a89aab8121efa944a2bbf
Grant Williams <grant@gwscripts.com>
2020-05-23 04:44:56 -0500

    Update routes for L7.

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\ServersController;
 use App\Http\Controllers\ServersTokenController;
 use App\Http\Controllers\UsersChangePasswordController;
 use App\Http\Controllers\UsersController;
+use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/dashboard');
 
