966959acc893f69fcf3b5880b701812b4b0971f9
Grant Williams <grant@gwscripts.com>
2020-05-24 00:57:55 -0500

    Use the password "secret" instead of "password".

--- tests/Factories/UserFactory.php
+++ tests/Factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends BaseFactory
         return [
             'name' => $faker->name,
             'email' => $faker->unique()->safeEmail,
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
             'remember_token' => Str::random(10),
         ];
 
