8f3f9de96aefdffe489bb30be7490fbf646659b7
Grant Williams <grant@gwscripts.com>
2020-05-24 00:36:03 -0500

    Add create logins table migration.

---
+++ database/migrations/2019_12_31_110000_create_logins_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLoginsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logins', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->index();
+            $table->string('ip_address');
+            $table->timestamp('created_at');
+        });
+    }
+}
