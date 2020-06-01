685df53731ba0093c2c776676b9e914bc845c420
Grant Williams <grant@gwscripts.com>
2020-06-01 04:24:54 -0500

    Update the Account unit tests.

--- tests/Unit/AccountTest.php
+++ tests/Unit/AccountTest.php
@@ -2,25 +2,28 @@
 
 namespace Tests\Unit;
 
+use App\Models\Server;
+use Tests\Factories\AccountFactory;
+use Tests\Factories\ServerFactory;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AccountTest extends TestCase
 {
     use RefreshDatabase;
-    
+
     /** @test */
     public function it_has_a_server()
     {
-        $account = create('App\Account');
+        $account = AccountFactory::new()->create();
 
-        $this->assertInstanceOf('App\Server', $account->server);
+        $this->assertInstanceOf(Server::class, $account->server);
     }
 
     /** @test */
     public function can_get_cpanel_external_url()
     {
-        $account = make('App\Account', ['domain' => 'mydomain.com']);
+        $account = AccountFactory::new()->make(['domain' => 'mydomain.com']);
 
         $this->assertEquals('https://mydomain.com/cpanel', $account->cpanel_url);
     }
@@ -28,8 +31,8 @@ class AccountTest extends TestCase
     /** @test */
     public function can_get_whm_external_url()
     {
-        $server = create('App\Server', ['address' => '1.1.1.1', 'port' => 2087]);
-        $account = make('App\Account', ['server_id' => $server->id, 'domain' => 'mydomain.com']);
+        $server = ServerFactory::new()->create(['address' => '1.1.1.1', 'port' => 2087]);
+        $account = AccountFactory::new()->make(['server_id' => $server->id, 'domain' => 'mydomain.com']);
 
         $this->assertEquals('https://1.1.1.1:2087', $account->whm_url);
     }
@@ -37,7 +40,7 @@ class AccountTest extends TestCase
     /** @test */
     public function can_get_domain_external_url()
     {
-        $account = make('App\Account', ['domain' => 'mydomain.com']);
+        $account = AccountFactory::new()->make(['domain' => 'mydomain.com']);
 
         $this->assertEquals('http://mydomain.com', $account->domain_url);
     }
@@ -45,9 +48,9 @@ class AccountTest extends TestCase
     /** @test */
     public function can_get_disk_usage()
     {
-        $accountA = make('App\Account', ['disk_used' => '300M', 'disk_limit' => '2000M']);
-        $accountB = make('App\Account', ['disk_used' => '350M', 'disk_limit' => '2000M']);
-        $accountC = make('App\Account', ['disk_used' => '400M', 'disk_limit' => 'unlimited']);
+        $accountA = AccountFactory::new()->make(['disk_used' => '300M', 'disk_limit' => '2000M']);
+        $accountB = AccountFactory::new()->make(['disk_used' => '350M', 'disk_limit' => '2000M']);
+        $accountC = AccountFactory::new()->make(['disk_used' => '400M', 'disk_limit' => 'unlimited']);
 
         $this->assertEquals('15%', $accountA->disk_usage);
         $this->assertEquals('17.5%', $accountB->disk_usage);
