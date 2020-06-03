b671cd1a68332df6f4b1f49f4074afc1eb1a4c7b
Grant Williams <grant@gwscripts.com>
2020-06-03 02:08:59 -0500

    WIP. First version of new styling app.css and app.js files. This will need to be cleaned up.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,25 +1,10 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+require('laravel-mix-purgecss');
 
-mix.js('resources/assets/js/app.js', 'public/js')
-    .extract(['axios', 'buefy', 'date-fns', 'laravel-echo', 'pusher-js', 'promise', 'vue'])
-    .copy('resources/assets/js/fontawesome/fontawesome.js', 'public/js/fontawesome.js')
-    .copy('resources/assets/js/fontawesome/packs/brands.js', 'public/js/fontawesome-brands.js')
-    .copy('resources/assets/js/fontawesome/packs/light.js', 'public/js/fontawesome-light.js')
-    .copy('resources/assets/js/fontawesome/packs/regular.js', 'public/js/fontawesome-regular.js')
-    .copy('resources/assets/js/fontawesome/packs/solid.js', 'public/js/fontawesome-solid.js')
-    .sass('resources/assets/sass/app.scss', 'public/css');
-
-if (mix.inProduction()) {
-    mix.version();
-}
+mix
+  .js('resources/js/app.js', 'public/js')
+  .postCss('resources/css/app.css', 'public/css', [require('tailwindcss')])
+  .purgeCss()
+  .version()
+  .sourceMaps();
