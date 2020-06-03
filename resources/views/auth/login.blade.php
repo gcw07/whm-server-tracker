5e4a95db5b4421ccd3b96ec376f60e88c15f19d1
Grant Williams <grant@gwscripts.com>
2020-06-03 14:52:20 -0500

    Update the login UI.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,65 +1,69 @@
-@extends('layouts.simple')
-
-@section('content')
-    <div class="columns">
-        <div class="column is-half is-offset-one-quarter">
-            <div class="box has-border-top">
-                <p class="title">
-                    Login
-                </p>
-
-                <hr>
-
-                <form role="form" method="POST" action="{{ route('login') }}">
-                    {{ csrf_field() }}
-
-                    <div class="field">
-                        <label class="label" for="email">E-Mail Address</label>
-                        <p class="control has-icons-left">
-                            <input id="email" type="email" class="input{{ $errors->has('email') ? ' is-danger' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-                            <span class="icon is-small is-left">
-                                <i class="fa fa-envelope"></i>
-                            </span>
-                            @if ($errors->has('email'))
-                                <p class="help is-danger">{{ $errors->first('email') }}</p>
-                            @endif
-                        </p>
-                    </div>
-
-                    <div class="field">
-                        <label class="label" for="password">Password</label>
-                        <p class="control has-icons-left">
-                            <input id="password" type="password" class="input{{ $errors->has('password') ? ' is-danger' : '' }}" name="password" required>
-                            <span class="icon is-small is-left">
-                                <i class="fa fa-lock"></i>
-                            </span>
-                            @if ($errors->has('password'))
-                                <p class="help is-danger">{{ $errors->first('password') }}</p>
-                            @endif
-                        </p>
-                    </div>
-
-                    <div class="field">
-                        <div class="control">
-                            <b-checkbox name="remember">Remember Me</b-checkbox>
-                        </div>
-                    </div>
-
-                    <div class="field is-grouped">
-                        <p class="control">
-                            <button class="button is-primary" type="submit">
-                                Login
-                            </button>
-                        </p>
-                        <p class="control">
-                            <a class="button is-link" href="{{ route('password.request') }}">
-                                Forgot Your Password?
-                            </a>
-                        </p>
-                    </div>
-                </form>
-
-            </div>
-        </div>
+<x-simple-layout>
+  <div class="relative bg-gray-200">
+    <div class="absolute inset-0 flex flex-col" aria-hidden="true">
+      <div class="flex-1 bg-gray-100"></div>
+      <div class="flex-1 bg-gray-200"></div>
     </div>
-@endsection
+    <div class="relative max-w-xl mx-auto">
+      <div class="flex flex-col min-h-screen sm:flex-row sm:items-center sm:p-8">
+        <div class="flex flex-col flex-grow bg-white sm:shadow-2xl sm:rounded-lg sm:overflow-hidden">
+          <div class="flex-grow flex flex-col sm:justify-center p-12">
+            <div>
+              <h1 class="text-gray-600 font-bold tracking-wide text-4xl text-center pb-5">Sign In</h1>
+            </div>
+            <div class="mt-3 pl-0">
+              <form role="form" method="POST" action="{{ route('login') }}">
+                {{ csrf_field() }}
+                <input type="hidden" name="remember" value="1">
+                <div>
+                  <label for="email">
+                    <span class="form-label">
+                        E-Mail Address
+                    </span>
+                    <span class="form-input-group">
+                      <span class="form-input-icon">
+                        <x-heroicon-s-mail />
+                      </span>
+                      <input class="form-input w-full {{ $errors->has('email') ? 'is-invalid' : '' }}" id="email"
+                             type="email" name="email" value="{{ old('email') }}" required autofocus>
+                    </span>
+                    @if ($errors->has('email'))
+                      <p class="invalid-feedback">{{ $errors->first('email') }}</p>
+                    @endif
+                  </label>
+                </div>
+                <div class="mt-4">
+                  <label for="password">
+                    <span class="form-label">
+                        Password
+                    </span>
+                    <span class="form-input-group">
+                      <span class="form-input-icon">
+                        <x-heroicon-s-lock-closed />
+                      </span>
+                      <input class="form-input w-full {{ $errors->has('password') ? 'is-invalid' : '' }}" id="password"
+                             type="password" name="password" required>
+                    </span>
+                    @if ($errors->has('password'))
+                      <p class="invalid-feedback">{{ $errors->first('password') }}</p>
+                    @endif
+                  </label>
+                </div>
+                <div class="mt-6 flex items-center justify-between">
+                  <button class="btn btn-blue w-full" type="submit">
+                    Sign In
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="bg-gray-100 border-t-2 border-gray-200 text-center p-4">
+            <a class="inline-block align-baseline" href="{{ route('password.request') }}">
+              Forgot Password?
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</x-simple-layout>
