4913d3c2c39b30294f1a0e88e2ca72f0d2e6c6c6
Grant Williams <grant@gwscripts.com>
2020-09-05 23:42:55 -0500

    Redesign login for new auth layout and use the blade ui kit elements for the form.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,56 @@
-<x-simple-layout>
-  <div class="relative bg-gray-200">
-    <div class="absolute inset-0 flex flex-col" aria-hidden="true">
-      <div class="flex-1 bg-gray-100"></div>
-      <div class="flex-1 bg-gray-200"></div>
+<x-layouts.auth title="Sign In">
+  <div class="flex flex-col flex-grow p-12 sm:justify-center">
+    <div>
+      <h1 class="pb-5 text-4xl font-bold tracking-wide text-center text-gray-600">Sign In</h1>
     </div>
-    <div class="relative max-w-xl mx-auto">
-      <div class="flex flex-col min-h-screen sm:flex-row sm:items-center sm:p-8">
-        <div class="flex flex-col flex-grow bg-white sm:shadow-2xl sm:rounded-lg sm:overflow-hidden">
-          <div class="flex-grow flex flex-col sm:justify-center p-12">
-            <div>
-              <h1 class="text-gray-600 font-bold tracking-wide text-4xl text-center pb-5">Sign In</h1>
-            </div>
-            <div class="mt-3 pl-0">
-              <form role="form" method="POST" action="{{ route('login') }}">
-                @csrf
-                <input type="hidden" name="remember" value="1">
-                <div>
-                  <label for="email">
-                    <span class="form-label">
-                        E-Mail Address
-                    </span>
-                    <span class="form-input-group">
-                      <span class="form-input-icon">
-                        <x-heroicon-s-mail />
-                      </span>
-                      <input class="form-input w-full {{ $errors->has('email') ? 'is-invalid' : '' }}" id="email"
-                             type="email" name="email" value="{{ old('email') }}" required autofocus>
-                    </span>
-                    @if ($errors->has('email'))
-                      <p class="invalid-feedback">{{ $errors->first('email') }}</p>
-                    @endif
-                  </label>
-                </div>
-                <div class="mt-4">
-                  <label for="password">
-                    <span class="form-label">
-                        Password
-                    </span>
-                    <span class="form-input-group">
-                      <span class="form-input-icon">
-                        <x-heroicon-s-lock-closed />
-                      </span>
-                      <input class="form-input w-full {{ $errors->has('password') ? 'is-invalid' : '' }}" id="password"
-                             type="password" name="password" required>
-                    </span>
-                    @if ($errors->has('password'))
-                      <p class="invalid-feedback">{{ $errors->first('password') }}</p>
-                    @endif
-                  </label>
-                </div>
-                <div class="mt-6 flex items-center justify-between">
-                  <button class="btn btn-blue w-full" type="submit">
-                    Sign In
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div class="bg-gray-100 border-t-2 border-gray-200 text-center p-4">
-            <a class="inline-block align-baseline" href="{{ route('password.request') }}">
-              Forgot Password?
-            </a>
-          </div>
+    <div class="pl-0 mt-3">
+
+      <x-form :action="route('login')">
+        <input type="hidden" name="remember" value="1">
+
+        <div>
+          <label for="email">
+            <span class="form-label">
+              E-Mail Address
+            </span>
+            <span class="form-input-group">
+              <span class="form-input-icon">
+                <x-heroicon-s-mail/>
+              </span>
+              <x-email name="email" required autofocus
+                       class="form-input w-full {{ $errors->has('email') ? 'is-invalid' : '' }}"/>
+            </span>
+            <x-error field="email" class="invalid-feedback"/>
+          </label>
         </div>
-      </div>
+        <div class="mt-4">
+          <label for="password">
+            <span class="form-label">
+              Password
+            </span>
+            <span class="form-input-group">
+              <span class="form-input-icon">
+                <x-heroicon-s-lock-closed/>
+              </span>
+              <x-password name="password" required
+                          class="form-input w-full {{ $errors->has('password') ? 'is-invalid' : '' }}"/>
+            </span>
+            <x-error field="password" class="invalid-feedback"/>
+          </label>
+        </div>
+        <div class="flex items-center justify-between mt-6">
+          <button class="w-full btn btn-blue" type="submit">
+            Sign In
+          </button>
+        </div>
+
+      </x-form>
+
     </div>
   </div>
-</x-simple-layout>
+  <div class="p-4 text-center bg-gray-100 border-t-2 border-gray-200">
+    <a class="inline-block align-baseline" href="{{ route('password.request') }}">
+      Forgot Password?
+    </a>
+  </div>
+</x-layouts.auth>
