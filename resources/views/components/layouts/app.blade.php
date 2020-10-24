03d10a26eb7303ab07c07368748da8e025aebaba
Grant Williams <grant@gwscripts.com>
2020-10-24 01:10:20 -0500

    Remove layout placeholder.

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -11,12 +11,6 @@
     </header>
     <main>
       <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        <!-- Replace with your content -->
-        <div class="px-4 py-4 sm:px-0">
-          <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
-        </div>
-        <!-- /End replace -->
-
         {{ $slot }}
       </div>
     </main>
