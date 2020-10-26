ed0f009cdd587487bd917eae145f0bd00375b1a7
Grant Williams <grant@gwscripts.com>
2020-10-26 00:51:48 -0500

    WIP. Customize the dropdown from blade UI kit.

---
+++ app/View/Components/Navigation/Dropdown.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\View\Components\Navigation;
+
+use BladeUIKit\Components\Navigation\Dropdown as OriginalDropdown;
+
+class Dropdown extends OriginalDropdown
+{
+    // Overwrite component parent here...
+}
