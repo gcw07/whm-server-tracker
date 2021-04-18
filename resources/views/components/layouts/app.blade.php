998e2c990a3c52777b9bb3b778e56767d821dd0d
Grant Williams <grant@gwscripts.com>
2021-04-18 04:06:36 -0500

    Remove the un-needed outer div.

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,18 +1,16 @@
 <x-layouts.base :title="$title">
   <x-layouts.navigation />
 
-  <div>
-    <header class="bg-white shadow">
-      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold leading-tight text-gray-900">
-          {{ $title }}
-        </h1>
-      </div>
-    </header>
-    <main>
-      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        {{ $slot }}
-      </div>
-    </main>
-  </div>
+  <header class="bg-white shadow">
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+      <h1 class="text-3xl font-bold leading-tight text-gray-900">
+        {{ $title }}
+      </h1>
+    </div>
+  </header>
+  <main>
+    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      {{ $slot }}
+    </div>
+  </main>
 </x-layouts.base>
