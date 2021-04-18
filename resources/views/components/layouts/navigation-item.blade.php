6ac53fd8fe9efe43ec77aa5c56654ed207f4c648
Grant Williams <grant@gwscripts.com>
2021-04-18 03:42:46 -0500

    Get the mobile navigation item updated.

--- resources/views/components/layouts/navigation-item.blade.php
+++ resources/views/components/layouts/navigation-item.blade.php
@@ -2,7 +2,7 @@
   if ($mobile) {
     $defaultClass = 'block px-3 py-2 rounded-md text-base font-medium';
     $activeClass = 'text-white bg-blue-500';
-    $inActiveClass = 'text-blue-200 hover:text-white hover:bg-blue-500';
+    $inActiveClass = 'text-white hover:bg-blue-500 hover:bg-opacity-75';
   } else {
     $defaultClass = 'px-3 py-2 rounded-md text-sm font-medium';
     $activeClass = 'text-white bg-blue-500';
