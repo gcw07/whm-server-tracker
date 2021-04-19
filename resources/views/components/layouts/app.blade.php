c44dad41bf7b9ff720c1a45956572b27f6701de3
Grant Williams <grant@gwscripts.com>
2021-04-19 04:38:57 -0500

    Add new "actions" slot to the header. This is where "new" links will go on listings pages.

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,10 +2,17 @@
   <x-layouts.navigation />
 
   <header class="bg-white shadow">
-    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold leading-tight text-gray-900">
-        {{ $title }}
-      </h1>
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex items-center justify-between">
+      <div class="flex-1 min-w-0">
+        <h1 class="text-3xl font-bold leading-tight text-gray-900">
+          {{ $title }}
+        </h1>
+      </div>
+      @if(isset($actions))
+        <div class="mt-4 flex md:mt-0 md:ml-4">
+          {{ $actions }}
+        </div>
+      @endif
     </div>
   </header>
   <main>
