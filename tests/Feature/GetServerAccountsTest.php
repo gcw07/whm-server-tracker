e0685c2ab70a6e3686e22b853a57ff01a166633e
Grant <grant@gwscripts.com>
2021-12-11 15:33:41 -0600

    Update get server accounts and get server details tests converting them to pest.

--- tests/Feature/GetServerAccountsTest.php
+++ tests/Feature/GetServerAccountsTest.php
@@ -1,52 +1,41 @@
 <?php
 
-namespace Tests\Feature;
-
 use App\Connectors\FakeServerConnector;
 use App\Connectors\ServerConnector;
 use App\Jobs\FetchServerAccounts;
 use App\Models\Server;
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Queue;
-use Tests\TestCase;
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 
-class GetServerAccountsTest extends TestCase
-{
-    use RefreshDatabase;
+uses(LazilyRefreshDatabase::class);
 
-    /** @test */
-    public function guests_can_not_fetch_server_accounts()
-    {
-        $server = Server::factory()->create();
+test('guests cannot fetch server accounts', function () {
+    $server = Server::factory()->create();
 
-        $this->get(route('servers.fetch-accounts', $server->id))
-            ->assertRedirect(route('login'));
-    }
+    $this->get(route('servers.fetch-accounts', $server->id))
+        ->assertRedirect(route('login'));
+});
 
-    /** @test */
-    public function an_authorized_user_can_fetch_server_accounts()
-    {
-        $user = User::factory()->create();
-        $server = Server::factory()->create([
-            'name'        => 'my-server-name',
-            'address'     => '1.1.1.1',
-            'port'        => 2087,
-            'server_type' => 'vps',
-            'token'       => 'valid-api-token',
-        ]);
+test('an authorized user can fetch server accounts', function () {
+    $user = User::factory()->create();
+    $server = Server::factory()->create([
+        'name' => 'my-server-name',
+        'address' => '1.1.1.1',
+        'port' => 2087,
+        'server_type' => 'vps',
+        'token' => 'valid-api-token',
+    ]);
 
-        Queue::fake();
+    Queue::fake();
 
-        $fake = new FakeServerConnector;
-        $this->app->instance(ServerConnector::class, $fake);
+    $fake = new FakeServerConnector;
+    $this->app->instance(ServerConnector::class, $fake);
 
-        $this->actingAs($user)
-            ->get(route('servers.fetch-accounts', $server->id))
-            ->assertSuccessful();
+    $this->actingAs($user)
+        ->get(route('servers.fetch-accounts', $server->id))
+        ->assertSuccessful();
 
-        Queue::assertPushed(FetchServerAccounts::class, function ($job) use ($server) {
-            return $job->server->is($server);
-        });
-    }
-}
+    Queue::assertPushed(FetchServerAccounts::class, function ($job) use ($server) {
+        return $job->server->is($server);
+    });
+});
--- tests/Feature/GetServerDetailsTest.php
+++ tests/Feature/GetServerDetailsTest.php
@@ -1,52 +1,41 @@
 <?php
 
-namespace Tests\Feature;
-
 use App\Connectors\FakeServerConnector;
 use App\Connectors\ServerConnector;
 use App\Jobs\FetchServerDetails;
 use App\Models\Server;
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Queue;
-use Tests\TestCase;
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 
-class GetServerDetailsTest extends TestCase
-{
-    use RefreshDatabase;
+uses(LazilyRefreshDatabase::class);
 
-    /** @test */
-    public function guests_can_not_fetch_server_details()
-    {
-        $server = Server::factory()->create();
+test('guests cannot fetch server details', function () {
+    $server = Server::factory()->create();
 
-        $this->get(route('servers.fetch-details', $server->id))
-            ->assertRedirect(route('login'));
-    }
+    $this->get(route('servers.fetch-details', $server->id))
+        ->assertRedirect(route('login'));
+});
 
-    /** @test */
-    public function an_authorized_user_can_fetch_server_details()
-    {
-        $user = User::factory()->create();
-        $server = Server::factory()->create([
-            'name'        => 'my-server-name',
-            'address'     => '1.1.1.1',
-            'port'        => 1000,
-            'server_type' => 'vps',
-            'token'       => 'valid-server-api-token',
-        ]);
+test('an authorized user can fetch server details', function () {
+    $user = User::factory()->create();
+    $server = Server::factory()->create([
+        'name' => 'my-server-name',
+        'address' => '1.1.1.1',
+        'port' => 1000,
+        'server_type' => 'vps',
+        'token' => 'valid-server-api-token',
+    ]);
 
-        Queue::fake();
+    Queue::fake();
 
-        $fake = new FakeServerConnector;
-        $this->app->instance(ServerConnector::class, $fake);
+    $fake = new FakeServerConnector;
+    $this->app->instance(ServerConnector::class, $fake);
 
-        $this->actingAs($user)
-            ->get(route('servers.fetch-details', $server->id))
-            ->assertSuccessful();
+    $this->actingAs($user)
+        ->get(route('servers.fetch-details', $server->id))
+        ->assertSuccessful();
 
-        Queue::assertPushed(FetchServerDetails::class, function ($job) use ($server) {
-            return $job->server->is($server);
-        });
-    }
-}
+    Queue::assertPushed(FetchServerDetails::class, function ($job) use ($server) {
+        return $job->server->is($server);
+    });
+});
