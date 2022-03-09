68ba8f13ee68a4dc71120e6c8ba89ba005acc1d2
Grant <grant@gwscripts.com>
2022-03-09 05:52:50 -0600

    WIP. Work on updating the server unit tests. Get majority of formatting testing working.

--- tests/Unit/ServerTest.php
+++ tests/Unit/ServerTest.php
@@ -20,53 +20,64 @@ it('a server has accounts', function () {
 });
 
 it('a server can add a setting', function () {
-    $this->server->settings()->set('disk_used', 100000);
-    $this->server->settings()->set('disk_available', 200000);
+    $this->server->settings['disk_used'] = 100000;
+    $this->server->settings['disk_available'] = 200000;
 
     $this->assertCount(2, $this->server->settings);
-    $this->assertEquals('100000', $this->server->settings()->get('disk_used'));
+    $this->assertEquals(100000, $this->server->settings['disk_used']);
 });
 
 it('a server can get a setting', function () {
-    $this->server->settings()->set('disk_available', 200000);
+    $this->server->settings['disk_available'] = 200000;
 
-    $this->assertEquals('200000', $this->server->settings()->get('disk_available'));
+    $this->assertEquals(200000, $this->server->settings['disk_available']);
 });
 
 it('a server can update a setting', function () {
-    $this->server->settings()->set('disk_available', 200000);
-    $this->assertEquals('200000', $this->server->settings()->get('disk_available'));
+    $this->server->settings['disk_available'] = 200000;
+    $this->assertEquals(200000, $this->server->settings['disk_available']);
 
-    $this->server->settings()->set('disk_available', 500000);
-    $this->assertEquals('500000', $this->server->settings()->get('disk_available'));
+    $this->server->settings['disk_available'] = 500000;
+    $this->assertEquals(500000, $this->server->settings['disk_available']);
+});
+
+it('a setting will not overwrite all settings when a single one is specified', function () {
+    $server = Server::factory()->create(['settings' => ['disk_available' => 500000]]);
+
+    $this->assertEquals(500000, $server->settings['disk_available']);
+    $server->settings['disk_used'] = 100000;
+
+    $server->save();
+    $server->refresh();
+
+    $this->assertEquals(500000, $server->settings['disk_available']);
+    $this->assertEquals(100000, $server->settings['disk_used']);
 });
 
 it('a server can update multiple settings at once', function () {
-    $this->server->settings()->merge([
+    $this->server->settings->merge([
         'disk_used' => 10000,
         'disk_available' => 200000,
     ]);
 
-    $this->assertEquals('10000', $this->server->settings()->get('disk_used'));
-    $this->assertEquals('200000', $this->server->settings()->get('disk_available'));
+    $this->assertEquals(10000, $this->server->settings['disk_used']);
+    $this->assertEquals(200000, $this->server->settings['disk_available']);
 });
 
 it('a server can remove a setting', function () {
-    $this->server->settings()->set('disk_available', 200000);
+    $this->server->settings['disk_available'] = 200000;
     $this->assertCount(1, $this->server->settings);
 
-    $this->server->settings()->forget('disk_available');
+    $this->server->settings->forget('disk_available');
     $this->assertCount(0, $this->server->settings);
 });
 
 it('a server can remove all settings', function () {
-    $this->server->settings()->set('disk_used', 10000);
-    $this->server->settings()->set('disk_available', 200000);
-
+    $this->server->settings['disk_used'] = 10000;
+    $this->server->settings['disk_available'] = 200000;
     $this->assertCount(2, $this->server->settings);
 
-    $this->server->settings()->forgetAll();
-
+    $this->server->settings->forgetAll();
     $this->assertCount(0, $this->server->settings);
 });
 
@@ -275,6 +286,14 @@ it('will only remove accounts that no longer exists on the server that is being 
     $this->assertEquals(2, Account::count());
 });
 
+it('can get whm url', function () {
+    $serverA = Server::factory()->make(['address' => '1.1.1.1', 'port' => 2087]);
+    $serverB = Server::factory()->make(['address' => '3.3.3.3', 'port' => 2086]);
+
+    $this->assertEquals('https://1.1.1.1:2087', $serverA->whm_url);
+    $this->assertEquals('http://3.3.3.3:2086', $serverB->whm_url);
+});
+
 it('can get formatted server types', function () {
     $serverA = Server::factory()->make(['server_type' => ServerTypeEnum::Vps]);
     $serverB = Server::factory()->make(['server_type' => ServerTypeEnum::Dedicated]);
@@ -286,14 +305,9 @@ it('can get formatted server types', function () {
 });
 
 it('can get formatted disk used', function () {
-    $serverA = Server::factory()->create();
-    $serverA->settings()->set('disk_used', '16305616');
-
-    $serverB = Server::factory()->create();
-    $serverB->settings()->set('disk_used', '204800');
-
-    $serverC = Server::factory()->create();
-    $serverC->settings()->set('disk_used', '570');
+    $serverA = Server::factory()->create(['settings' => ['disk_used' => '16305616']]);
+    $serverB = Server::factory()->create(['settings' => ['disk_used' => '204800']]);
+    $serverC = Server::factory()->create(['settings' => ['disk_used' => '570']]);
 
     $this->assertEquals('15.55 GB', $serverA->formatted_disk_used);
     $this->assertEquals('200 MB', $serverB->formatted_disk_used);
@@ -301,9 +315,7 @@ it('can get formatted disk used', function () {
 });
 
 it('can get formatted disk available', function () {
-    $serverA = Server::factory()->create();
-    $serverA->settings()->set('disk_available', '109523504');
-
+    $serverA = Server::factory()->create(['settings' => ['disk_available' => '109523504']]);
     $serverB = Server::factory()->create();
 
     $this->assertEquals('104.45 GB', $serverA->formatted_disk_available);
@@ -311,13 +323,111 @@ it('can get formatted disk available', function () {
 });
 
 it('can get formatted disk total', function () {
-    $serverA = Server::factory()->create();
-    $serverA->settings()->set('disk_total', '125829120');
-
+    $serverA = Server::factory()->create(['settings' => ['disk_total' => '125829120']]);
     $serverB = Server::factory()->create();
 
     $this->assertEquals('120 GB', $serverA->formatted_disk_total);
     $this->assertEquals('Unknown', $serverB->formatted_disk_total);
+});
+
+it('can get formatted backup daily days', function () {
+    $serverA = Server::factory()->create(['settings' => ['backup_daily_days' => '0,1,2']]);
+    $serverB = Server::factory()->create(['settings' => ['backup_daily_days' => '3,4,5,6']]);
+    $serverC = Server::factory()->create(['settings' => ['backup_daily_days' => '0,2,4,6']]);
+    $serverD = Server::factory()->create();
+
+    $this->assertEquals('Sun,Mon,Tue', $serverA->formatted_backup_daily_days);
+    $this->assertEquals('Wed,Thu,Fri,Sat', $serverB->formatted_backup_daily_days);
+    $this->assertEquals('Sun,Tue,Thu,Sat', $serverC->formatted_backup_daily_days);
+    $this->assertEquals('None', $serverD->formatted_backup_daily_days);
+});
+
+it('can get formatted backup weekly day', function () {
+    $serverA = Server::factory()->create(['settings' => ['backup_weekly_day' => '0']]);
+    $serverB = Server::factory()->create(['settings' => ['backup_weekly_day' => '3']]);
+    $serverC = Server::factory()->create(['settings' => ['backup_weekly_day' => '6']]);
+    $serverD = Server::factory()->create();
+
+    $this->assertEquals('Sunday', $serverA->formatted_backup_weekly_day);
+    $this->assertEquals('Wednesday', $serverB->formatted_backup_weekly_day);
+    $this->assertEquals('Saturday', $serverC->formatted_backup_weekly_day);
+    $this->assertEquals('None', $serverD->formatted_backup_weekly_day);
+});
+
+it('can get formatted backup monthly days', function () {
+    $serverA = Server::factory()->create(['settings' => ['backup_monthly_days' => '1,15']]);
+    $serverB = Server::factory()->create(['settings' => ['backup_monthly_days' => '1']]);
+    $serverC = Server::factory()->create(['settings' => ['backup_monthly_days' => '15']]);
+    $serverD = Server::factory()->create();
+
+    $this->assertEquals('1st,15th', $serverA->formatted_backup_monthly_days);
+    $this->assertEquals('1st', $serverB->formatted_backup_monthly_days);
+    $this->assertEquals('15th', $serverC->formatted_backup_monthly_days);
+    $this->assertEquals('None', $serverD->formatted_backup_monthly_days);
+});
+
+it('can get formatted php installed versions', function () {
+    $serverA = Server::factory()->create(['settings' => ['php_installed_versions' => 'ea-php54']]);
+    $serverB = Server::factory()->create(['settings' => ['php_installed_versions' => 'ea-php55']]);
+    $serverC = Server::factory()->create(['settings' => ['php_installed_versions' => 'ea-php56']]);
+    $serverD = Server::factory()->create(['settings' => ['php_installed_versions' => 'ea-php70']]);
+    $serverE = Server::factory()->create(['settings' => ['php_installed_versions' => 'ea-php71']]);
+    $serverF = Server::factory()->create(['settings' => ['php_installed_versions' => 'ea-php72']]);
+    $serverG = Server::factory()->create(['settings' => ['php_installed_versions' => 'ea-php73']]);
+    $serverH = Server::factory()->create(['settings' => ['php_installed_versions' => 'ea-php74']]);
+    $serverI = Server::factory()->create(['settings' => ['php_installed_versions' => 'ea-php80']]);
+    $serverJ = Server::factory()->create(['settings' => ['php_installed_versions' => 'ea-php81']]);
+    $serverK = Server::factory()->create(['settings' => ['php_installed_versions' => ['ea-php74','ea-php80']]]);
+    $serverL = Server::factory()->create();
+
+    $this->assertEquals('5.4', $serverA->formatted_php_installed_versions);
+    $this->assertEquals('5.5', $serverB->formatted_php_installed_versions);
+    $this->assertEquals('5.6', $serverC->formatted_php_installed_versions);
+    $this->assertEquals('7.0', $serverD->formatted_php_installed_versions);
+    $this->assertEquals('7.1', $serverE->formatted_php_installed_versions);
+    $this->assertEquals('7.2', $serverF->formatted_php_installed_versions);
+    $this->assertEquals('7.3', $serverG->formatted_php_installed_versions);
+    $this->assertEquals('7.4', $serverH->formatted_php_installed_versions);
+    $this->assertEquals('8.0', $serverI->formatted_php_installed_versions);
+    $this->assertEquals('8.1', $serverJ->formatted_php_installed_versions);
+    $this->assertEquals('8.1', $serverK->formatted_php_installed_versions);
+    $this->assertEquals('Unknown', $serverL->formatted_php_installed_versions);
+});
+
+it('can get formatted php system version', function () {
+    $serverA = Server::factory()->create(['settings' => ['php_system_version' => 'ea-php54']]);
+    $serverB = Server::factory()->create(['settings' => ['php_system_version' => 'ea-php55']]);
+    $serverC = Server::factory()->create(['settings' => ['php_system_version' => 'ea-php56']]);
+    $serverD = Server::factory()->create(['settings' => ['php_system_version' => 'ea-php70']]);
+    $serverE = Server::factory()->create(['settings' => ['php_system_version' => 'ea-php71']]);
+    $serverF = Server::factory()->create(['settings' => ['php_system_version' => 'ea-php72']]);
+    $serverG = Server::factory()->create(['settings' => ['php_system_version' => 'ea-php73']]);
+    $serverH = Server::factory()->create(['settings' => ['php_system_version' => 'ea-php74']]);
+    $serverI = Server::factory()->create(['settings' => ['php_system_version' => 'ea-php80']]);
+    $serverJ = Server::factory()->create(['settings' => ['php_system_version' => 'ea-php81']]);
+    $serverK = Server::factory()->create();
+
+    $this->assertEquals('5.4', $serverA->formatted_php_system_version);
+    $this->assertEquals('5.5', $serverB->formatted_php_system_version);
+    $this->assertEquals('5.6', $serverC->formatted_php_system_version);
+    $this->assertEquals('7.0', $serverD->formatted_php_system_version);
+    $this->assertEquals('7.1', $serverE->formatted_php_system_version);
+    $this->assertEquals('7.2', $serverF->formatted_php_system_version);
+    $this->assertEquals('7.3', $serverG->formatted_php_system_version);
+    $this->assertEquals('7.4', $serverH->formatted_php_system_version);
+    $this->assertEquals('8.0', $serverI->formatted_php_system_version);
+    $this->assertEquals('8.1', $serverJ->formatted_php_system_version);
+    $this->assertEquals('Unknown', $serverK->formatted_php_system_version);
+});
+
+it('can get formatted whm version', function () {
+    $serverA = Server::factory()->create(['settings' => ['whm_version' => '11.100.0.11']]);
+    $serverB = Server::factory()->create(['settings' => ['whm_version' => '11.88.0.9999']]);
+    $serverC = Server::factory()->create();
+
+    $this->assertEquals('v100.0.11', $serverA->formatted_whm_version);
+    $this->assertEquals('v88.0.9999', $serverB->formatted_whm_version);
+    $this->assertEquals('Unknown', $serverC->formatted_whm_version);
 });
 
 it('can determine a missing api token', function () {
@@ -338,80 +448,8 @@ it('can determine if it can refresh external data', function () {
     $this->assertFalse($serverTypeNeedsNoToken->can_refresh_data);
 });
 
-it('can get whm external url', function () {
-    $serverA = Server::factory()->make(['address' => '1.1.1.1', 'port' => 2087]);
-    $serverB = Server::factory()->make(['address' => '3.3.3.3', 'port' => 2086]);
-
-    $this->assertEquals('https://1.1.1.1:2087', $serverA->whm_url);
-    $this->assertEquals('http://3.3.3.3:2086', $serverB->whm_url);
-});
-
 it('the api token should not be included in returned data', function () {
     $server = Server::factory()->make(['server_type' => ServerTypeEnum::Vps, 'token' => 'valid-token']);
 
     $this->assertArrayNotHasKey('token', $server->toArray());
-});
-
-it('can get formatted backup days', function () {
-    $serverA = Server::factory()->create();
-    $serverA->settings()->set('backup_days', '0,1,2');
-
-    $serverB = Server::factory()->create();
-    $serverB->settings()->set('backup_days', '3,4,5,6');
-
-    $serverC = Server::factory()->create();
-    $serverC->settings()->set('backup_days', '0,2,4,6');
-
-    $serverD = Server::factory()->create();
-
-    $this->assertEquals('Sun,Mon,Tue', $serverA->formatted_backup_days);
-    $this->assertEquals('Wed,Thu,Fri,Sat', $serverB->formatted_backup_days);
-    $this->assertEquals('Sun,Tue,Thu,Sat', $serverC->formatted_backup_days);
-    $this->assertEquals('None', $serverD->formatted_backup_days);
-});
-
-it('can get formatted php version', function () {
-    $serverA = Server::factory()->create();
-    $serverA->settings()->set('php_version', 'ea-php54');
-
-    $serverB = Server::factory()->create();
-    $serverB->settings()->set('php_version', 'ea-php55');
-
-    $serverC = Server::factory()->create();
-    $serverC->settings()->set('php_version', 'ea-php56');
-
-    $serverD = Server::factory()->create();
-    $serverD->settings()->set('php_version', 'ea-php70');
-
-    $serverE = Server::factory()->create();
-    $serverE->settings()->set('php_version', 'ea-php71');
-
-    $serverF = Server::factory()->create();
-    $serverF->settings()->set('php_version', 'ea-php72');
-
-    $serverG = Server::factory()->create();
-    $serverG->settings()->set('php_version', 'ea-php73');
-
-    $serverH = Server::factory()->create();
-    $serverH->settings()->set('php_version', 'ea-php74');
-
-    $serverI = Server::factory()->create();
-    $serverI->settings()->set('php_version', 'ea-php80');
-
-    $serverJ = Server::factory()->create();
-    $serverJ->settings()->set('php_version', 'ea-php81');
-
-    $serverK = Server::factory()->create();
-
-    $this->assertEquals('PHP 5.4', $serverA->formatted_php_version);
-    $this->assertEquals('PHP 5.5', $serverB->formatted_php_version);
-    $this->assertEquals('PHP 5.6', $serverC->formatted_php_version);
-    $this->assertEquals('PHP 7.0', $serverD->formatted_php_version);
-    $this->assertEquals('PHP 7.1', $serverE->formatted_php_version);
-    $this->assertEquals('PHP 7.2', $serverF->formatted_php_version);
-    $this->assertEquals('PHP 7.3', $serverG->formatted_php_version);
-    $this->assertEquals('PHP 7.4', $serverH->formatted_php_version);
-    $this->assertEquals('PHP 8.0', $serverI->formatted_php_version);
-    $this->assertEquals('PHP 8.1', $serverJ->formatted_php_version);
-    $this->assertEquals('Unknown', $serverK->formatted_php_version);
 });
