ddb82f4a604d7ca112953a66f4ae2f952fa65337
Grant <grant@gwscripts.com>
2022-03-20 04:09:16 -0500

    Clean up and remove the old configuration settings.

--- config/server-tracker.php
+++ config/server-tracker.php
@@ -26,24 +26,6 @@ return [
         'connection_timeout' => 10,
     ],
 
-    'urls' => [
-        'protocol' => 'https',
-        'api-path' => 'json-api',
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Remote Server Defaults
-    |--------------------------------------------------------------------------
-    | This is the server username and the number of seconds before timing out
-    | on server requests. The timeout is in seconds.
-    */
-
-    'remote' => [
-        'username' => 'root',
-        'timeout' => 10,
-    ],
-
     /*
     |--------------------------------------------------------------------------
     | Ignore Usernames
