b26cd192f91093b57231e4bfb2661dd00b481481
Grant <grant@gwscripts.com>
2022-03-26 21:52:29 -0500

    Add new FetchedDataFailed and FetchedDataSucceeded events.

---
+++ app/Events/FetchedDataSucceededEvent.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Server;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class FetchedDataSucceededEvent implements ShouldQueue
+{
+    public Server $server;
+    public array $messages;
+
+    public function __construct(Server $server, array $messages)
+    {
+        $this->server = $server;
+
+        $this->messages = $messages;
+    }
+}
