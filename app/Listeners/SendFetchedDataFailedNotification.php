e11359ea8c95c937769fc441c36b7a3c831bb7f7
Grant <grant@gwscripts.com>
2022-03-28 03:41:41 -0500

    Update the listeners to send a notification.

--- app/Listeners/SendFetchedDataFailedNotification.php
+++ app/Listeners/SendFetchedDataFailedNotification.php
@@ -2,29 +2,23 @@
 
 namespace App\Listeners;
 
+use App\Events\FetchedDataFailedEvent;
+use App\Notifications\FetchedDataFailed;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Notification;
 
 class SendFetchedDataFailedNotification
 {
-    /**
-     * Create the event listener.
-     *
-     * @return void
-     */
     public function __construct()
     {
         //
     }
 
-    /**
-     * Handle the event.
-     *
-     * @param  object  $event
-     * @return void
-     */
-    public function handle($event)
+    public function handle(FetchedDataFailedEvent $event)
     {
-        //
+        Notification::route('mail', config('server-tracker.notifications.mail.to'))
+            ->route('slack', config('server-tracker.notifications.slack.webhook_url'))
+            ->notify(new FetchedDataFailed($event));
     }
 }
