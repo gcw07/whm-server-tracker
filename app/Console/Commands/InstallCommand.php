8d15d06c03d9a9ae697539a984475e1914a2b35d
Grant <grant@gwscripts.com>
2022-04-04 03:02:52 -0500

    We don't need the construct called when it is doing nothing in install and refresh commands.

--- app/Console/Commands/InstallCommand.php
+++ app/Console/Commands/InstallCommand.php
@@ -24,16 +24,6 @@ class InstallCommand extends Command
     protected $description = 'Install the Server Tracker application.';
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
      * Execute the console command.
      *
      * @return mixed
--- app/Console/Commands/RefreshCommand.php
+++ app/Console/Commands/RefreshCommand.php
@@ -22,16 +22,6 @@ class RefreshCommand extends Command
     protected $description = 'Refresh remote server data.';
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
      * Execute the console command.
      *
      * @return mixed
