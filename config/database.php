315d20cce786c1ae238f2e9a2573e58b311670db
Grant <grant@gwscripts.com>
2022-04-04 03:02:24 -0500

    Add new database connection to the old v1 database.

--- config/database.php
+++ config/database.php
@@ -63,6 +63,26 @@ return [
             ]) : [],
         ],
 
+        'mysql_v1_db' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('WHM_V1_DB_HOST', '127.0.0.1'),
+            'port' => env('WHM_V1_DB_PORT', '3306'),
+            'database' => env('WHM_V1_DB_DATABASE', 'forge'),
+            'username' => env('WHM_V1_DB_USERNAME', 'forge'),
+            'password' => env('WHM_V1_DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
