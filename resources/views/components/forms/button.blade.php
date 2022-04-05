73bd427859ab18614a0dc3966adcea58c2cdeb4a
Grant <grant@gwscripts.com>
2022-04-05 04:35:18 -0500

    Update the button classes.

--- resources/views/components/forms/button.blade.php
+++ resources/views/components/forms/button.blade.php
@@ -1,5 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-blue-500 border border-transparent rounded-md font-semibold text-sm text-white tracking-wide hover:bg-blue-400 active:bg-blue-50 active:text-blue-800 focus:outline-none focus:border-blue-900 focus:ring focus:ring-blue-600 disabled:opacity-25 transition duration-150 ease-in-out']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500']) }}>
   {{ $slot }}
 </button>
-
-inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500
