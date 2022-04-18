4a8b2d7f902a1c74f980f5b6ab10b2398e1cd765
Grant <grant@gwscripts.com>
2022-04-18 01:02:56 -0500

    Switch over to the new dropdown component in the navigation menu.

--- resources/views/components/layouts/navigation.blade.php
+++ resources/views/components/layouts/navigation.blade.php
@@ -109,34 +109,20 @@
 
       <div class="hidden lg:ml-4 lg:flex lg:items-center">
         <!-- Profile dropdown -->
-        <div class="ml-3 relative flex-shrink-0">
-          <div>
+        <x-navigation.dropdown class="ml-3 flex-shrink-0 x-cloak">
+          <x-slot name="trigger">
             <button type="button" class="max-w-xs bg-sky-600 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 lg:p-2 lg:rounded-md lg:hover:bg-sky-500" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
               <x-heroicon-s-user-circle class="h-5 w-5 text-white" />
               <span class="hidden ml-2 text-white text-sm font-medium lg:block"><span class="sr-only">Open user menu for </span>Emilia Birch</span>
               <x-heroicon-s-chevron-down class="hidden flex-shrink-0 ml-1 h-5 w-5 text-white lg:block" />
             </button>
+          </x-slot>
+
+          <div class="x-cloak origin-top-right z-20 absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1" id="user-menu-item-0">Manager Users</a>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
           </div>
-
-          <!--
-            Dropdown menu, show/hide based on menu state.
-
-            Entering: "transition ease-out duration-100"
-              From: "transform opacity-0 scale-95"
-              To: "transform opacity-100 scale-100"
-            Leaving: "transition ease-in duration-75"
-              From: "transform opacity-100 scale-100"
-              To: "transform opacity-0 scale-95"
-          -->
-          <div class="hidden origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-            <!-- Active: "bg-gray-100", Not Active: "" -->
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
-          </div>
-        </div>
+        </x-navigation.dropdown>
       </div>
     </div>
   </div>
