854cd113b986f3bfa33ec388c6dab5138ae68717
Grant <grant@gwscripts.com>
2022-05-31 22:46:27 -0500

    Get the full page pagination UI working.

--- resources/views/livewire/pagination/index.blade.php
+++ resources/views/livewire/pagination/index.blade.php
@@ -1,19 +1,24 @@
 <nav class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6" aria-label="Pagination">
   <div class="hidden sm:block">
     <p class="text-sm text-gray-700">
-      Showing
-      <span class="font-medium">{{ $paginator->firstItem() }}</span>
-      to
-      <span class="font-medium">{{ $paginator->lastItem() }}</span>
-      of
-      <span class="font-medium">{{ $paginator->total() }}</span>
-      results
+      @if ($paginator->hasPages())
+        Showing
+        <span class="font-medium">{{ $paginator->firstItem() }}</span>
+        to
+        <span class="font-medium">{{ $paginator->lastItem() }}</span>
+        of
+        <span class="font-medium">{{ $paginator->total() }}</span>
+        results
+      @else
+        <span class="font-medium">0</span>
+        results
+      @endif
     </p>
   </div>
   <div class="flex-1 flex justify-between sm:justify-end">
     @if ($paginator->onFirstPage())
-      <span class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 cursor-not-allowed">
-        Previous (disabled)
+      <span class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-400 bg-white cursor-not-allowed">
+        Previous
       </span>
     @else
       <button wire:click="previousPage" wire:loading.attr="disabled" class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">
@@ -26,8 +31,8 @@
         Next
       </button>
     @else
-      <span class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">
-        Next (disabled)
+      <span class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-400 bg-white cursor-not-allowed">
+        Next
       </span>
     @endif
   </div>
