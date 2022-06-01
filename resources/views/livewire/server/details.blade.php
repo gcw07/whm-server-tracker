73fa399500686473493c1375af6b64b33f5f135f
Grant <grant@gwscripts.com>
2022-06-01 06:12:00 -0500

    Get external link on details page working.

--- resources/views/livewire/server/details.blade.php
+++ resources/views/livewire/server/details.blade.php
@@ -31,10 +31,10 @@
       <!--            Edit-->
       <!--          </button>-->
 
-      <button type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">
+      <a href="{{ $server->whm_url }}" target="_blank" type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">
         <x-heroicon-s-external-link class="-ml-0.5 mr-2 h-4 w-4" />
         View
-      </button>
+      </a>
 
       <button type="button" class="ml-2 inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">
         <x-heroicon-s-refresh class="-ml-0.5 mr-2 h-4 w-4" />
