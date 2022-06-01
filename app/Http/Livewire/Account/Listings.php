7e3669b5e26f7a3932100b09592cc882c256329e
Grant <grant@gwscripts.com>
2022-06-01 01:55:01 -0500

    Use pagination for account listings.

--- app/Http/Livewire/Account/Listings.php
+++ app/Http/Livewire/Account/Listings.php
@@ -4,19 +4,25 @@ namespace App\Http\Livewire\Account;
 
 use App\Models\Account;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Listings extends Component
 {
-    public $accounts;
+    use WithPagination;
 
     public function mount()
     {
-        $this->accounts = Account::with(['server'])->orderBy('domain')->get();
     }
 
     public function render()
     {
-        return view('livewire.account.listings')
-            ->layoutData(['title' => 'Accounts']);
+        return view('livewire.account.listings', [
+            'accounts' => $this->query(),
+        ])->layoutData(['title' => 'Accounts']);
+    }
+
+    protected function query()
+    {
+        return Account::query()->with(['server'])->orderBy('domain')->paginate(50);
     }
 }
