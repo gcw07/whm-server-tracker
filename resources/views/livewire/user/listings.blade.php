f31cd43415c7647f1f9437dbc9fe64c76bcbf2e2
Grant <grant@gwscripts.com>
2022-06-02 02:22:04 -0500

    Get hiding of the columns working for smaller screen sizes on user listings UI.

--- resources/views/livewire/user/listings.blade.php
+++ resources/views/livewire/user/listings.blade.php
@@ -94,14 +94,14 @@
                     <td class="px-6 py-4 text-center whitespace-nowrap text-sm text-gray-500">
                       <span class="text-gray-900 font-medium">{{ $user->email }}</span>
                     </td>
-                    <td class="px-6 py-4 text-center whitespace-nowrap text-sm text-gray-500">
+                    <td class="hidden px-6 py-4 text-center whitespace-nowrap text-sm text-gray-500 lg:table-cell">
                       <span class="text-gray-900 font-medium">{{ $user->lastLogin?->created_at->format('F j, Y, g:i a') }}</span>
                     </td>
-                    <td class="px-6 py-4 text-center whitespace-nowrap text-sm text-gray-500">
+                    <td class="hidden px-6 py-4 text-center whitespace-nowrap text-sm text-gray-500 lg:table-cell">
                       <span class="text-gray-900 font-medium">{{ $user->lastLogin?->ip_address }}</span>
                     </td>
                     <td class="px-6 py-4 text-right whitespace-nowrap text-sm text-gray-500">
-                      <a href="#" target="_blank" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">
+                      <a href="#" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">
                         <x-heroicon-s-dots-vertical class="-ml-0.5 h-4 w-4"/>
                       </a>
                     </td>
