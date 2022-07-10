2154cfb6eeb1d1f835346a28a7ac282577744774
Grant <grant@gwscripts.com>
2022-07-10 02:49:51 -0500

    Add labels to server type enum.

--- app/Enums/ServerTypeEnum.php
+++ app/Enums/ServerTypeEnum.php
@@ -7,4 +7,13 @@ enum ServerTypeEnum: string
     case Dedicated = 'dedicated';
     case Reseller = 'reseller';
     case Vps = 'vps';
+
+    public static function labels(): array
+    {
+        return [
+            'dedicated' => 'Dedicated',
+            'reseller' => 'Reseller',
+            'vps' => 'VPS',
+        ];
+    }
 }
