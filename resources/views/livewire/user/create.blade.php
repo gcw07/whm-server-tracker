93477d7d882a57721bfe383fb0308fdeeb3fef42
Grant <grant@gwscripts.com>
2022-07-10 00:29:49 -0500

    Add new user create livewire component. Get it fully working.

--- resources/views/livewire/user/create.blade.php
+++ resources/views/livewire/user/create.blade.php
@@ -14,32 +14,44 @@
 
     <div class="shadow bg-white px-6 py-4 sm:rounded-lg">
 
-      <form class="space-y-8 divide-y divide-gray-200">
+      <form wire:submit.prevent="save" class="space-y-8 divide-y divide-gray-200">
         <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
           <div>
             <div>
-              <h3 class="text-lg leading-6 font-medium text-gray-900">Personal Information</h3>
-              <p class="mt-1 max-w-2xl text-sm text-gray-500">Use a permanent address where you can receive mail.</p>
+              <h3 class="text-lg leading-6 font-medium text-gray-900 mb-1">Personal Information</h3>
             </div>
             <div class="space-y-6 sm:space-y-5">
               <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                <label for="first-name" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Name </label>
+{{--                <x-forms.label for="name" :required="true" />--}}
+{{--                <x-forms.text-input name="name" class="sm:mt-0 sm:col-span-2"></x-forms.text-input>--}}
+                <label for="name" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Name </label>
                 <div class="mt-1 sm:mt-0 sm:col-span-2">
-                  <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md">
+                  <input type="text" wire:model="state.name" id="name" autocomplete="given-name" class="block max-w-lg w-full shadow-sm focus:ring-sky-500 focus:border-sky-500 sm:text-sm border-gray-300 rounded-md">
+                  @error('state.name') <span class="mt-2 text-sm text-red-600">{{ $message }}</span> @enderror
                 </div>
               </div>
 
               <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                 <label for="email" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Email address </label>
                 <div class="mt-1 sm:mt-0 sm:col-span-2">
-                  <input id="email" name="email" type="email" autocomplete="email" class="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md">
+                  <input id="email" wire:model="state.email" type="email" autocomplete="email" class="block max-w-lg w-full shadow-sm focus:ring-sky-500 focus:border-sky-500 sm:text-sm border-gray-300 rounded-md">
+                  @error('state.email') <span class="mt-2 text-sm text-red-600">{{ $message }}</span> @enderror
                 </div>
               </div>
 
               <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                <label for="street-address" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Password </label>
+                <label for="password" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Password </label>
                 <div class="mt-1 sm:mt-0 sm:col-span-2">
-                  <input type="password" name="street-address" id="street-address" autocomplete="street-address" class="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md">
+                  <input type="password" wire:model="state.password" id="password" class="block max-w-lg w-full shadow-sm focus:ring-sky-500 focus:border-sky-500 sm:text-sm border-gray-300 rounded-md">
+                  @error('state.password') <span class="mt-2 text-sm text-red-600">{{ $message }}</span> @enderror
+                </div>
+              </div>
+
+              <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                <label for="password_confirmation" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Confirm Password </label>
+                <div class="mt-1 sm:mt-0 sm:col-span-2">
+                  <input type="password" wire:model="state.password_confirmation" id="password_confirmation" class="block max-w-lg w-full shadow-sm focus:ring-sky-500 focus:border-sky-500 sm:text-sm border-gray-300 rounded-md">
+                  @error('state.password_confirmation') <span class="mt-2 text-sm text-red-600">{{ $message }}</span> @enderror
                 </div>
               </div>
             </div>
@@ -48,8 +60,8 @@
 
         <div class="pt-5">
           <div class="flex justify-end">
-            <button type="button" class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Cancel</button>
-            <button type="submit" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
+            <a href="{{ route('users.index') }}" class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">Cancel</a>
+            <button type="submit" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">Save</button>
           </div>
         </div>
       </form>
