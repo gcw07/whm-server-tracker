612bc8eafe2f21b40bf9f219958736a369d8a0e6
Grant <grant@gwscripts.com>
2022-07-11 01:09:28 -0500

    Move livewire modal include in the app.js above the alpine include.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
+require('../../vendor/wire-elements/modal/resources/js/modal');
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 Alpine.start();
-
-require('../../vendor/wire-elements/modal/resources/js/modal');
