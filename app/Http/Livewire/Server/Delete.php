e6b5a3349b700334b4cc8bccf04db179ecca2be4
Grant <grant@gwscripts.com>
2022-07-11 02:20:08 -0500

    Get the delete server modal working.

--- app/Http/Livewire/Server/Delete.php
+++ app/Http/Livewire/Server/Delete.php
@@ -2,12 +2,32 @@
 
 namespace App\Http\Livewire\Server;
 
+use App\Models\Server;
 use LivewireUI\Modal\ModalComponent;
 
 class Delete extends ModalComponent
 {
+    public $server;
+
+    public function mount(Server $server)
+    {
+        $this->server = $server;
+    }
+
     public function render()
     {
         return view('livewire.server.delete');
+    }
+
+    public static function modalMaxWidth(): string
+    {
+        return 'xl';
+    }
+
+    public function delete()
+    {
+        $this->server->delete();
+
+        return redirect()->route('servers.index');
     }
 }
