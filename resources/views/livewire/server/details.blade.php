bade236c8fe6a3ae1059cdab37eae18246cd6dd6
Grant <grant@gwscripts.com>
2022-07-13 03:21:43 -0500

    Get link to edit server working on server details page.

--- resources/views/livewire/server/details.blade.php
+++ resources/views/livewire/server/details.blade.php
@@ -53,7 +53,7 @@
         <div
           class="origin-top-right z-50 absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
           role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-          <a href="{{ route('users.index') }}" class="flex items-center group px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+          <a href="{{ route('servers.edit', $server) }}" class="flex items-center group px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
              role="menuitem" tabindex="-1" id="details-menu-item-0">
             <x-heroicon-s-pencil-alt class="mr-3 h-4 w-4 text-gray-400 group-hover:text-gray-500" />
             Edit
