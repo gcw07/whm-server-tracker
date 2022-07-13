732dcad3c52c39486dc04e27df00760da05526d6
Grant <grant@gwscripts.com>
2022-07-13 02:19:08 -0500

    When clicking on an item within a dropdown, trigger a close. This fixes the problem with the dropdown remaining open when triggering a modal.

--- resources/views/components/navigation/dropdown.blade.php
+++ resources/views/components/navigation/dropdown.blade.php
@@ -4,6 +4,7 @@
   </div>
 
   <div x-show="open"
+       @click="open = false"
        x-transition:enter="transition ease-out duration-100"
        x-transition:enter-start="transform opacity-0 scale-95"
        x-transition:enter-end="transform opacity-100 scale-100"
