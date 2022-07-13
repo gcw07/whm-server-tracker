3433b232deab9c78b77df3e32c3336f478baedb0
Grant <grant@gwscripts.com>
2022-07-13 03:26:10 -0500

    Add an edit server component.

---
+++ app/Http/Livewire/Server/Edit.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Livewire\Server;
+
+use App\Enums\ServerTypeEnum;
+use App\Models\Server;
+use Illuminate\Validation\Rules\Enum;
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public Server $server;
+
+    /**
+     * The component's state.
+     */
+    public array $state = [];
+
+    protected $validationAttributes = [
+        'state.name' => 'name',
+        'state.address' => 'address',
+        'state.port' => 'port',
+        'state.server_type' => 'server type',
+        'state.notes' => 'notes',
+    ];
+
+    public function mount(Server $server)
+    {
+        $this->server = $server;
+
+        $this->state = $server->only(['name', 'address', 'port', 'server_type', 'notes']);
+    }
+
+    public function render()
+    {
+        return view('livewire.server.edit')->layoutData(['title' => 'Edit Server']);
+    }
+
+    protected function rules(): array
+    {
+        return [
+            'state.name' => ['required', 'string', 'max:255'],
+            'state.address' => ['required', 'string', 'max:255'],
+            'state.port' => ['required', 'numeric'],
+            'state.server_type' => ['required', new Enum(ServerTypeEnum::class)],
+            'state.notes' => ['nullable', 'string'],
+        ];
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        $this->server->update($this->state);
+
+        return redirect()->route('servers.show', $this->server);
+    }
+}
