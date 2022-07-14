9d535c52a245b4ea4c4017ab096cb84854cebe26
Grant <grant@gwscripts.com>
2022-07-14 14:46:56 -0500

    Set the cancel button focus ring to sky blue.

--- resources/views/livewire/user/change-password.blade.php
+++ resources/views/livewire/user/change-password.blade.php
@@ -34,7 +34,7 @@
     </div>
     <div class="mt-8 sm:mt-4 sm:flex sm:flex-row-reverse">
       <button type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-sky-600 text-base font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:ml-3 sm:w-auto sm:text-sm">Save</button>
-      <button wire:click="cancel" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm">Cancel</button>
+      <button wire:click="cancel" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:mt-0 sm:w-auto sm:text-sm">Cancel</button>
     </div>
   </form>
 </div>
