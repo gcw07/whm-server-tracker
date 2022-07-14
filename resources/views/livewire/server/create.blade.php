6a31b9f2285d10e9c684a87ddf91ef0d569bd8b9
Grant <grant@gwscripts.com>
2022-07-14 14:22:45 -0500

    Update the label for the api token on the create server page. Also add extra description of the field.

--- resources/views/livewire/server/create.blade.php
+++ resources/views/livewire/server/create.blade.php
@@ -67,9 +67,10 @@
               </div>
 
               <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                <label for="token" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Token </label>
+                <label for="token" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> API Token </label>
                 <div class="mt-1 sm:mt-0 sm:col-span-2">
                   <input type="text" wire:model="state.token" id="token" class="block max-w-lg w-full shadow-sm focus:ring-sky-500 focus:border-sky-500 sm:text-sm border-gray-300 rounded-md">
+                  <p class="text-xs text-gray-500">This is a WHM API Token. Once set this will not be visible again.</p>
                   @error('state.token') <span class="mt-2 text-sm text-red-600">{{ $message }}</span> @enderror
                 </div>
               </div>
