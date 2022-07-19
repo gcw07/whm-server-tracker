19da7014584b6cbe08444380049d717fbe242d34
Grant <grant@gwscripts.com>
2022-07-19 15:01:28 -0500

    Add new test to view server details to test refreshing server data button. This will queue refreshing the data from the remote server.

--- tests/Feature/ViewServerDetailsTest.php
+++ tests/Feature/ViewServerDetailsTest.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Livewire\Server\Details as ServerDetails;
+use App\Jobs\FetchServerDataJob;
 use App\Models\Server;
 use App\Models\User;
+use App\Services\WHM\WhmApi;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+use Tests\Factories\WhmApiFake;
 
 uses(LazilyRefreshDatabase::class);
 
@@ -21,4 +24,28 @@ test('an authorized user can view server details page', function () {
 
     Livewire::test(ServerDetails::class, ['server' => $server])
         ->assertSee('MyServer.com');
+});
+
+test('an authorized user can refresh server data from server details', function () {
+    $server = Server::factory()->create([
+        'name' => 'MyServer.com',
+        'address' => '1.1.1.1',
+        'port' => 1000,
+        'server_type' => 'vps',
+        'token' => 'valid-server-api-token',
+    ]);
+
+    Queue::fake();
+
+    $fake = new WhmApiFake;
+    $this->app->instance(WhmApi::class, $fake);
+
+    $this->actingAs(User::factory()->create());
+
+    Livewire::test(ServerDetails::class, ['server' => $server])
+        ->call('refresh');
+
+    Queue::assertPushed(FetchServerDataJob::class, function ($job) use ($server) {
+        return $job->server->is($server);
+    });
 });
