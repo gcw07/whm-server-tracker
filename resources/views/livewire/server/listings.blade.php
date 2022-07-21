d6f9bf47f3a91cc1677b60d6e3ba683da7956f8d
Grant <grant@gwscripts.com>
2022-07-21 14:45:07 -0500

    Add tooltip for viewing whm panel links.

--- resources/views/livewire/server/listings.blade.php
+++ resources/views/livewire/server/listings.blade.php
@@ -210,7 +210,7 @@
                       <span class="text-gray-900 font-medium">{{ $server->settings->get('disk_percentage') }}%</span>
                     </td>
                     <td class="px-6 py-4 text-right whitespace-nowrap text-sm text-gray-500">
-                      <a href="{{ $server->whm_url }}" target="_blank" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">
+                      <a href="{{ $server->whm_url }}" target="_blank" x-data="{}" x-tooltip.raw="View WHM Panel" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">
                         <x-heroicon-s-external-link class="-ml-0.5 h-4 w-4" />
                       </a>
                     </td>
