8ea0d33dde9753313bf71ecd772b68717d9c3eeb
Grant <grant@gwscripts.com>
2022-07-21 14:37:17 -0500

    Install a tooltip alpine plugin.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,11 @@
 require('../../vendor/wire-elements/modal/resources/js/modal');
 
 import Alpine from 'alpinejs';
+import Tooltip from "@ryangjchandler/alpine-tooltip";
 import ToastComponent from '../../vendor/usernotnull/tall-toasts/dist/js/tall-toasts'
 import SearchComponent from './search';
+
+Alpine.plugin(Tooltip);
 
 Alpine.data('ToastComponent', ToastComponent);
 Alpine.data('SearchComponent', SearchComponent);
