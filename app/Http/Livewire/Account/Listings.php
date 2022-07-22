063baae670f74a164fc72f2662a310ed0f54a87e
Grant <grant@gwscripts.com>
2022-07-22 16:48:43 -0500

    Add to account listings the ability to sort the entries by various options. Add special disk usage calculation for sorting by as it is normally done in a presenter.

--- app/Http/Livewire/Account/Listings.php
+++ app/Http/Livewire/Account/Listings.php
@@ -2,16 +2,20 @@
 
 namespace App\Http\Livewire\Account;
 
+use App\Http\Livewire\WithCache;
 use App\Models\Account;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class Listings extends Component
 {
-    use WithPagination;
+    use WithPagination, WithCache;
+
+    public string|null $sortBy = null;
 
     public function mount()
     {
+        $this->sortBy = $this->getCache('accounts', 'sortBy');
     }
 
     public function render()
@@ -21,8 +25,38 @@ class Listings extends Component
         ])->layoutData(['title' => 'Accounts']);
     }
 
+    public function sortListingsBy($name)
+    {
+        $this->sortBy = match ($name) {
+            'newest' => 'newest',
+            'usage_high' => 'usage_high',
+            'usage_low' => 'usage_low',
+            default => null,
+        };
+
+        $this->putCache('accounts','sortBy', $this->sortBy);
+    }
+
     protected function query()
     {
-        return Account::query()->with(['server'])->orderBy('domain')->paginate(50);
+        return Account::query()
+            ->with(['server'])
+            ->select('*')
+            ->selectRaw('(disk_used / disk_limit) * 100 as sort_disk_usage')
+            ->when($this->sortBy, function ($query) {
+                if ($this->sortBy === 'newest') {
+                    return $query->orderBy('created_at', 'DESC');
+                }
+
+                if ($this->sortBy === 'usage_high') {
+                    return $query->orderBy('sort_disk_usage', 'DESC');
+                }
+
+                // usage low
+                return $query->orderBy('sort_disk_usage', 'ASC');
+            }, function($query) {
+                return $query->orderBy('domain');
+            })
+            ->paginate(50);
     }
 }
