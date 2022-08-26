9aa16fad50898290b2365f6f3f4c29b0497ea4f4
Grant <grant@gwscripts.com>
2022-08-26 03:34:12 -0500

    Add the reason the uptime check failed when the site is down.

--- resources/views/livewire/monitor/details.blade.php
+++ resources/views/livewire/monitor/details.blade.php
@@ -99,6 +99,12 @@
                   <dt class="text-sm font-medium text-gray-500">Last Checked</dt>
                   <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $monitor->uptime_last_check_date?->diffForHumans() }}</dd>
                 </div>
+                @if($monitor->uptime_status === 'down')
+                  <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">Failure Reason</dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $monitor->uptime_check_failure_reason }}</dd>
+                  </div>
+                @endif
               </dl>
             </div>
           </div>
