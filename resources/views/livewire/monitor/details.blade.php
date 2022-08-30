746ff0e5629867e051145a8e713cd6d5e4890bd9
Grant <grant@gwscripts.com>
2022-08-30 02:46:16 -0500

    Switch external link icon to be the mini one.

--- resources/views/livewire/monitor/details.blade.php
+++ resources/views/livewire/monitor/details.blade.php
@@ -24,7 +24,7 @@
 
     <div class="flex mt-3 md:mt-0 md:ml-4">
       <a href="{{ $monitor->url }}" target="_blank" type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">
-        <x-heroicon-s-arrow-top-right-on-square class="-ml-0.5 mr-2 h-4 w-4" />
+        <x-heroicon-m-arrow-top-right-on-square class="-ml-0.5 mr-2 h-4 w-4" />
         View
       </a>
     </div>
@@ -377,7 +377,7 @@
                     </td>
                     <td class="px-6 py-4 text-right whitespace-nowrap text-sm text-gray-500">
                       <a href="{{ $account->server->whm_url }}" target="_blank" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">
-                        <x-heroicon-s-arrow-top-right-on-square class="-ml-0.5 h-4 w-4" />
+                        <x-heroicon-m-arrow-top-right-on-square class="-ml-0.5 h-4 w-4" />
                       </a>
                     </td>
                   </tr>
--- resources/views/livewire/monitor/listings.blade.php
+++ resources/views/livewire/monitor/listings.blade.php
@@ -228,7 +228,7 @@
                     </td>
                     <td class="px-6 py-4 text-right whitespace-nowrap text-sm text-gray-500">
                       <a href="{{ $monitor->url }}" target="_blank" x-data="{}" x-tooltip.raw="Visit Site" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">
-                        <x-heroicon-s-arrow-top-right-on-square class="-ml-0.5 h-4 w-4" />
+                        <x-heroicon-m-arrow-top-right-on-square class="-ml-0.5 h-4 w-4" />
                       </a>
                     </td>
                   </tr>
