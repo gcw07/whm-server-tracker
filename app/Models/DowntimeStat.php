79d9c5f9cf1eb34d2688b175cdcab6d1724b9bac
Grant <grant@gwscripts.com>
2022-09-01 02:50:28 -0500

    Add model docblock to new DowntimeStat model.

--- app/Models/DowntimeStat.php
+++ app/Models/DowntimeStat.php
@@ -5,6 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\UptimeMonitor\Models\Monitor;
 
+/**
+ * App\Models\DowntimeStat
+ *
+ * @property int $id
+ * @property int $monitor_id
+ * @property \Illuminate\Support\Carbon $date
+ * @property int $downtime_period
+ * @property-read Monitor $monitor
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|DowntimeStat newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|DowntimeStat newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|DowntimeStat query()
+ * @method static \Illuminate\Database\Eloquent\Builder|DowntimeStat whereDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|DowntimeStat whereDowntimePeriod($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|DowntimeStat whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|DowntimeStat whereMonitorId($value)
+ * @mixin \Eloquent
+ */
 class DowntimeStat extends Model
 {
     protected $guarded = [];
