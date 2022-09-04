79cfa05f92dfb94086203c146a0d078f96f900a3
Grant <grant@gwscripts.com>
2022-09-04 15:00:32 -0500

    Use the MonitorFactory from the UptimeMonitor package instead of local model in downtime stat factory.

--- database/factories/DowntimeStatFactory.php
+++ database/factories/DowntimeStatFactory.php
@@ -2,15 +2,15 @@
 
 namespace Database\Factories;
 
-use App\Models\Monitor;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Spatie\UptimeMonitor\Database\Factories\MonitorFactory;
 
 class DowntimeStatFactory extends Factory
 {
     public function definition(): array
     {
         return [
-            'monitor_id' => Monitor::factory(),
+            'monitor_id' => MonitorFactory::new(),
             'date' => $this->faker->date(),
             'downtime_period' => $this->faker->numberBetween(0, 3600),
         ];
