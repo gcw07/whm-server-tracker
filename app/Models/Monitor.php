f5a3e6c4cae30f01e0cefe6e32219bb716bd14e1
Grant <grant@gwscripts.com>
2022-09-03 21:34:36 -0500

    Add a new monitor model. This will extend the uptime-checker base monitor model.

---
+++ app/Models/Monitor.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\UptimeMonitor\Models\Monitor as BaseMonitor;
+
+/**
+ * App\Models\Monitor
+ *
+ * @property int $id
+ * @property \Spatie\Url\Url|null $url
+ * @property bool $uptime_check_enabled
+ * @property string $look_for_string
+ * @property string $uptime_check_interval_in_minutes
+ * @property string $uptime_status
+ * @property string|null $uptime_check_failure_reason
+ * @property int $uptime_check_times_failed_in_a_row
+ * @property \Illuminate\Support\Carbon|null $uptime_status_last_change_date
+ * @property \Illuminate\Support\Carbon|null $uptime_last_check_date
+ * @property \Illuminate\Support\Carbon|null $uptime_check_failed_event_fired_on_date
+ * @property string $uptime_check_method
+ * @property string|null $uptime_check_payload
+ * @property array $uptime_check_additional_headers
+ * @property string|null $uptime_check_response_checker
+ * @property bool $certificate_check_enabled
+ * @property string $certificate_status
+ * @property \Illuminate\Support\Carbon|null $certificate_expiration_date
+ * @property string|null $certificate_issuer
+ * @property string $certificate_check_failure_reason
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read string $certificate_status_as_emoji
+ * @property-read string $chunked_last_certificate_check_failure_reason
+ * @property-read string $chunked_last_failure_reason
+ * @property-read string $raw_url
+ * @property-read string $uptime_status_as_emoji
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\DowntimeStat[] $stats
+ * @property-read int|null $stats_count
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor enabled()
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereCertificateCheckEnabled($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereCertificateCheckFailureReason($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereCertificateExpirationDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereCertificateIssuer($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereCertificateStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereLookForString($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereUptimeCheckAdditionalHeaders($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereUptimeCheckEnabled($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereUptimeCheckFailedEventFiredOnDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereUptimeCheckFailureReason($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereUptimeCheckIntervalInMinutes($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereUptimeCheckMethod($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereUptimeCheckPayload($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereUptimeCheckResponseChecker($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereUptimeCheckTimesFailedInARow($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereUptimeLastCheckDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereUptimeStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereUptimeStatusLastChangeDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Monitor whereUrl($value)
+ * @mixin \Eloquent
+ */
+class Monitor extends BaseMonitor
+{
+    public function stats(): HasMany
+    {
+        return $this->hasMany(DowntimeStat::class);
+    }
+}
