f16162e9781fb3beee2104f09e8db4c5b89723ab
Grant <grant@gwscripts.com>
2022-09-23 03:46:51 -0500

    Add the base notification class. This is a copy of the Spatie one with a few modifications.

---
+++ app/Notifications/BaseNotification.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Notifications\Notification;
+use Spatie\UptimeMonitor\Models\Enums\CertificateStatus;
+use Spatie\UptimeMonitor\Models\Monitor;
+
+abstract class BaseNotification extends Notification
+{
+    public function getMonitor(): Monitor
+    {
+        return $this->event->monitor;
+    }
+
+    public function getMonitorProperties($extraProperties = []): array
+    {
+        $monitor = $this->getMonitor();
+
+        $properties = array_merge([], $extraProperties);
+
+        if ($monitor->certificate_check_enabled && $monitor->certificate_status === CertificateStatus::VALID) {
+            $certificateTitle = "Certificate expires in {$monitor->formattedCertificateExpirationDate('forHumans')}";
+            $certificateIssuer = $monitor->certificate_issuer;
+
+            $properties[$certificateTitle] = $certificateIssuer;
+        }
+
+        return array_filter($properties);
+    }
+
+    public function getLocationDescription(): string
+    {
+        $configuredLocation = config('uptime-monitor.notifications.location');
+
+        if ($configuredLocation == '') {
+            return '';
+        }
+
+        return "Monitor {$configuredLocation}";
+    }
+
+    public function isStillRelevant(): bool
+    {
+        return true;
+    }
+}
