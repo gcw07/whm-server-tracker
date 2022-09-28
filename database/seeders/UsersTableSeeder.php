a306fe96bb8fc22bc5a319e689b36d611caa3940
Grant <grant@gwscripts.com>
2022-09-28 12:53:23 -0500

    Add notification type settings to the user table seeder.

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -14,10 +14,20 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::factory()->create([
+        User::factory()->create([
             'name' => 'Grant Williams',
             'email' => 'grant@example.com',
             'password' => bcrypt('secret'),
+            'notification_types' => [
+                'uptime_check_failed' => false,
+                'uptime_check_succeeded' => false,
+                'uptime_check_recovered' => false,
+                'certificate_check_succeeded' => false,
+                'certificate_check_failed' => false,
+                'certificate_expires_soon' => false,
+                'fetched_server_data_succeeded' => false,
+                'fetched_server_data_failed' => false,
+            ]
         ]);
     }
 }
