c58115b9f0d0d7f970ec41621118ccfcf0d22daf
Grant <grant@gwscripts.com>
2022-09-28 01:52:39 -0500

    Add new notifications cast. This will be used to save the types of notifications a user wants to receive. Add new notifications collection.

---
+++ app/Casts/Notifications.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Casts;
+
+use App\Collections\NotificationsCollection;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+
+class Notifications implements CastsAttributes
+{
+    protected array $allowed = [
+        'uptime_check_failed',
+        'uptime_check_succeeded',
+        'uptime_check_recovered',
+        'certificate_check_succeeded',
+        'certificate_check_failed',
+        'certificate_expires_soon',
+        'fetched_server_data_succeeded',
+        'fetched_server_data_failed',
+    ];
+
+    public function get($model, $key, $value, $attributes): ?NotificationsCollection
+    {
+        return isset($attributes[$key]) ? new NotificationsCollection(json_decode($attributes[$key], true)) : new NotificationsCollection([]);
+    }
+
+    public function set($model, $key, $value, $attributes): array
+    {
+        if (! $value instanceof NotificationsCollection) {
+            $value = new NotificationsCollection($value);
+        }
+
+        return [$key => json_encode($value->only($this->allowed))];
+    }
+}
---
+++ app/Collections/NotificationsCollection.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Collections;
+
+use Illuminate\Support\Collection;
+
+class NotificationsCollection extends Collection
+{
+    public function merge($items): static
+    {
+        $this->items = array_merge($this->items, $this->getArrayableItems($items));
+
+        return $this;
+    }
+
+    public function forgetAll(): static
+    {
+        $this->each(fn ($item, $key) => $this->forget($key));
+
+        return $this;
+    }
+}
