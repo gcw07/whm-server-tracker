e397252da6750558d855855ee61ae473d5d971b4
Grant <grant@gwscripts.com>
2022-10-18 01:43:21 -0500

    Add to the search livewire component a new search to search monitors.

--- app/Http/Livewire/Search.php
+++ app/Http/Livewire/Search.php
@@ -3,7 +3,9 @@
 namespace App\Http\Livewire;
 
 use App\Models\Account;
+use App\Models\Monitor;
 use App\Models\Server;
+use Illuminate\Support\Collection;
 use Livewire\Component;
 
 class Search extends Component
@@ -24,6 +26,7 @@ class Search extends Component
         return view('livewire.search', [
             'servers' => $this->searchServers($term),
             'accounts' => $this->searchAccounts($term),
+            'monitors' => $this->searchMonitors($term),
         ])->layoutData(['title' => 'Search']);
     }
 
@@ -32,7 +35,7 @@ class Search extends Component
         $this->reset('q');
     }
 
-    protected function searchServers($term)
+    protected function searchServers($term): Collection
     {
         if (! empty($term)) {
             return Server::query()->withCount(['accounts'])->search($term)->orderBy('name')->get();
@@ -41,10 +44,19 @@ class Search extends Component
         return collect();
     }
 
-    protected function searchAccounts($term)
+    protected function searchAccounts($term): Collection
     {
         if (! empty($term)) {
             return Account::query()->with(['server'])->search($term)->orderBy('domain')->get();
+        }
+
+        return collect();
+    }
+
+    protected function searchMonitors($term): Collection
+    {
+        if (! empty($term)) {
+            return Monitor::query()->search($term)->orderBy('url')->get();
         }
 
         return collect();
