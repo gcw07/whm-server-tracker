38cb63f820c81571e4301269f5193aa31e0fa925
Grant <grant@gwscripts.com>
2022-10-30 12:47:25 -0500

    Add a new button to the monitor details UI to refresh the certificate check..

--- resources/views/livewire/monitor/details.blade.php
+++ resources/views/livewire/monitor/details.blade.php
@@ -27,6 +27,11 @@
         <x-heroicon-m-arrow-top-right-on-square class="-ml-0.5 mr-2 h-4 w-4" />
         View
       </a>
+
+      <button wire:click="refreshCertificateCheck" type="button" class="ml-2 inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">
+        <x-heroicon-s-arrow-path class="-ml-0.5 mr-2 h-4 w-4" />
+        Refresh
+      </button>
     </div>
   </div>
   <!-- / End Page Header -->
