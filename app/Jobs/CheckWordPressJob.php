029b68f8ec0df233425a91c3f4b010db07e79689
Grant <grant@gwscripts.com>
2022-11-28 02:07:36 -0600

    Add new check wordpress job. This will actually do the checking in a queue.

---
+++ app/Jobs/CheckWordPressJob.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Account;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class CheckWordPressJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public Account $account;
+
+    public int $tries = 5;
+
+    public function __construct(Account $account)
+    {
+        $this->account = $account;
+    }
+
+    public function handle()
+    {
+        $this->account->checkWordPress();
+    }
+}
