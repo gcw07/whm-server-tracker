9b0fe514ef9a205c47b073c77490b887bf5e694a
Grant <grant@gwscripts.com>
2022-12-28 00:56:21 -0600

    Add new table migration for enabling or disabling lighthouse checks.

---
+++ database/migrations/2022_12_18_000000_add_lighthouse_check_to_monitors_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Enums\LighthouseStatusEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('monitors', function (Blueprint $table) {
+            $table->after('blacklist_check_failure_reason', function (Blueprint $table) {
+                $table->boolean('lighthouse_check_enabled')->default(true);
+                $table->string('lighthouse_status')->default(LighthouseStatusEnum::NotYetChecked->value);
+                $table->timestamp('lighthouse_update_last_failed_at')->nullable();
+                $table->timestamp('lighthouse_update_last_succeeded_at')->nullable();
+            });
+        });
+    }
+};
