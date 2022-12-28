0a293c44ab76b163910f69555d2dcb2418b99230
Grant <grant@gwscripts.com>
2022-12-28 02:37:16 -0600

    WIP. Add a create lighthouse audits table migration. Might need to add additional fields.

---
+++ database/migrations/2022_12_19_000000_create_lighthouse_audits_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateLighthouseAuditsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lighthouse_audits', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('monitor_id');
+            $table->date('date');
+
+            $table->integer('performance_score');
+            $table->integer('accessibility_score');
+            $table->integer('best_practices_score');
+            $table->integer('seo_score');
+            $table->integer('pwa_score');
+            $table->longText('report')->nullable();
+            $table->timestamps();
+
+            $table->foreign('monitor_id')->references('id')->on('monitors')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('monitors');
+    }
+}
