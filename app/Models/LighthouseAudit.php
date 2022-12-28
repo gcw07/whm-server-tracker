a4866305b621192a5f4169a20c93d5874b91231b
Grant <grant@gwscripts.com>
2022-12-28 02:37:36 -0600

    Add docblock to lighthouse audit model.

--- app/Models/LighthouseAudit.php
+++ app/Models/LighthouseAudit.php
@@ -6,6 +6,40 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\UptimeMonitor\Models\Monitor;
 
+/**
+ * App\Models\LighthouseAudit
+ *
+ * @property int $id
+ * @property int $monitor_id
+ * @property \Illuminate\Support\Carbon $date
+ * @property int $performance_score
+ * @property int $accessibility_score
+ * @property int $best_practices_score
+ * @property int $seo_score
+ * @property int $pwa_score
+ * @property string|null $report
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read Monitor $monitor
+ *
+ * @method static \Database\Factories\LighthouseAuditFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|LighthouseAudit newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|LighthouseAudit newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|LighthouseAudit query()
+ * @method static \Illuminate\Database\Eloquent\Builder|LighthouseAudit whereAccessibilityScore($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LighthouseAudit whereBestPracticesScore($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LighthouseAudit whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LighthouseAudit whereDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LighthouseAudit whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LighthouseAudit whereMonitorId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LighthouseAudit wherePerformanceScore($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LighthouseAudit wherePwaScore($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LighthouseAudit whereReport($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LighthouseAudit whereSeoScore($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LighthouseAudit whereUpdatedAt($value)
+ *
+ * @mixin \Eloquent
+ */
 class LighthouseAudit extends Model
 {
     use HasFactory;
