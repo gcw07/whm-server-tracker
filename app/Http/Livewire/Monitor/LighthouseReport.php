c0b57ccfae2743b6109c8bbcc70a45aa397e039d
Grant <grant@gwscripts.com>
2023-01-30 02:15:42 -0600

    Update the lighthouse report page. Show all audits. Add new way to change the selected audit report.

--- app/Http/Livewire/Monitor/LighthouseReport.php
+++ app/Http/Livewire/Monitor/LighthouseReport.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Monitor;
 
+use App\Models\LighthouseAudit;
 use App\Models\Monitor;
 use Livewire\Component;
 
@@ -9,17 +10,24 @@ class LighthouseReport extends Component
 {
     public Monitor $monitor;
 
-    protected string $domainUrl;
+    public $audits;
+
+    public $selectedAudit;
 
     public function mount(Monitor $monitor)
     {
         $this->monitor = $monitor;
+        $this->audits = $this->monitor->lighthouseLatestAudit()->get();
+        $this->selectedAudit = $this->audits->first();
     }
 
     public function render()
     {
-        return view('livewire.monitor.reports', [
-            'monitor' => $this->monitor,
-        ])->layoutData(['title' => 'Monitors']);
+        return view('livewire.monitor.reports', [])->layoutData(['title' => 'Monitors']);
+    }
+
+    public function changeSelected(LighthouseAudit $audit)
+    {
+        $this->selectedAudit = $audit;
     }
 }
