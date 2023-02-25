3bac316f190a96d3f4b250d1fc4fecdb486df2b6
Grant <grant@gwscripts.com>
2023-02-25 01:38:03 -0600

    Add new job that checks the domain name expiration date.

---
+++ app/Jobs/CheckDomainNameJob.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Monitor;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class CheckDomainNameJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public Monitor $monitor;
+
+    public int $tries = 5;
+
+    public function __construct(Monitor $monitor)
+    {
+        $this->monitor = $monitor;
+    }
+
+    public function handle()
+    {
+        $this->monitor->checkDomainNameExpiration();
+    }
+}
