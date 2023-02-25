5395c6279f95430ffd6394b60a511ebeb67ec0f3
Grant <grant@gwscripts.com>
2023-02-25 03:29:41 -0600

    Formatting.

--- app/Models/Monitor.php
+++ app/Models/Monitor.php
@@ -337,21 +337,21 @@ class Monitor extends BaseMonitor
     }
 
         public function setDomainNameExpiration($date): void
-    {
-        $this->domain_name_status = DomainNameStatusEnum::Valid->value;
-        $this->domain_name_expiration_date = $date;
-        $this->domain_name_check_failure_reason = null;
-        $this->save();
+        {
+            $this->domain_name_status = DomainNameStatusEnum::Valid->value;
+            $this->domain_name_expiration_date = $date;
+            $this->domain_name_check_failure_reason = null;
+            $this->save();
 
 //        event(new DomainNameCheckSucceeded($this, $exception->getMessage()));
-    }
+        }
 
         public function setDomainNameException(Exception $exception): void
-    {
-        $this->domain_name_status = DomainNameStatusEnum::Invalid->value;
-        $this->domain_name_check_failure_reason = $exception->getMessage();
-        $this->save();
+        {
+            $this->domain_name_status = DomainNameStatusEnum::Invalid->value;
+            $this->domain_name_check_failure_reason = $exception->getMessage();
+            $this->save();
 
 //        event(new DomainNameCheckFailed($this, $exception->getMessage()));
-    }
+        }
 }
