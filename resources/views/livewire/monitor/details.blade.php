c22a23a2820be5747b1c8526b8ad27b5122096d9
Grant <grant@gwscripts.com>
2023-07-28 15:39:32 -0500

    When blacklist entries are found, show each entry 1 per line on the details page.

--- resources/views/livewire/monitor/details.blade.php
+++ resources/views/livewire/monitor/details.blade.php
@@ -339,7 +339,9 @@
                   @if($monitor->blacklist_status === 'invalid')
                     <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                       <dt class="text-sm font-medium text-gray-500">List</dt>
-                      <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $monitor->blacklist_check_failure_reason }}</dd>
+                      <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        {!! nl2br($monitor->blacklist_check_failure_reason) !!}
+                      </dd>
                     </div>
                   @endif
                 </dl>
