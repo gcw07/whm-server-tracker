8d5a525fb10f3ccf5b17b86c43ac7eb822530853
Grant <grant@gwscripts.com>
2023-08-29 13:36:13 -0500

    Add a send domain name expires soon listener that sends the domain name expires soon notification.

---
+++ app/Listeners/SendDomainNameExpiresSoon.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\DomainNameExpiresSoonEvent;
+use App\Models\User;
+use Illuminate\Support\Facades\Notification;
+
+class SendDomainNameExpiresSoon
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function handle(DomainNameExpiresSoonEvent $event)
+    {
+        $users = User::forNotificationType('domain_name_expires_soon')->get();
+        $notification = new \App\Notifications\DomainNameExpiresSoon($event);
+
+        if ($notification->isStillRelevant()) {
+            Notification::send($users, $notification);
+        }
+    }
+}
