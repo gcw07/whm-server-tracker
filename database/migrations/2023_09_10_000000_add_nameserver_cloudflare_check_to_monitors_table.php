623c8c3aac00c0d13671c5841d06b93dd9d9fc04
Grant <grant@gwscripts.com>
2023-09-27 01:26:15 -0500

    Add new table migration for adding nameserver data and if the site is on cloudflare.

---
+++ database/migrations/2023_09_10_000000_add_nameserver_cloudflare_check_to_monitors_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Enums\DomainNameStatusEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('monitors', function (Blueprint $table) {
+            $table->after('domain_name_check_failure_reason', function (Blueprint $table) {
+                $table->json('nameservers')->nullable();
+                $table->boolean('is_on_cloudflare')->default(false);
+            });
+        });
+    }
+};
