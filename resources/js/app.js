80520272dfb2233584228b39be6568cde85e9535
Grant <grant@gwscripts.com>
2023-12-29 16:20:17 -0600

    Update the app.js file for new Livewire 3.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,13 @@
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import '../../vendor/wire-elements/modal/resources/js/modal';
 
-import Alpine from 'alpinejs';
-import focus from '@alpinejs/focus';
 import Tooltip from "@ryangjchandler/alpine-tooltip";
 import ToastComponent from '../../vendor/usernotnull/tall-toasts/resources/js/tall-toasts';
 import SearchComponent from './search';
 
-Alpine.plugin(focus);
 Alpine.plugin(Tooltip);
 
 Alpine.data('ToastComponent', ToastComponent);
 Alpine.data('SearchComponent', SearchComponent);
 
-window.Alpine = Alpine;
-Alpine.start();
+Livewire.start();
