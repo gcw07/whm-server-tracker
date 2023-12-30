e53379a419df3bc484f9f049aa677a02725d4c1e
Grant <grant@gwscripts.com>
2023-12-30 02:12:49 -0600

    Latest laravel version doesn't like static password for tests. Use Hash::make instead.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -12,7 +13,7 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+            'password' => Hash::make('secret'),
             'remember_token' => Str::random(10),
         ];
     }
