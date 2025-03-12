0a2537f6d99bf6b16285248bd811392e92bd41bb
Grant <grant@gwscripts.com>
2025-03-12 03:25:06 -0500

    Add php 8.4. Also add new server default 7.4 code.

--- app/Models/Presenters/ServerPresenter.php
+++ app/Models/Presenters/ServerPresenter.php
@@ -154,11 +154,12 @@ trait ServerPresenter
                             'ea-php71' => '7.1',
                             'ea-php72' => '7.2',
                             'ea-php73' => '7.3',
-                            'ea-php74' => '7.4',
+                            'ea-php74', 'nf-php74' => '7.4',
                             'ea-php80' => '8.0',
                             'ea-php81' => '8.1',
                             'ea-php82' => '8.2',
                             'ea-php83' => '8.3',
+                            'ea-php84' => '8.4',
                             default => 'Unknown'
                         })->toArray();
                 }
@@ -186,6 +187,7 @@ trait ServerPresenter
                         'ea-php81' => '8.1',
                         'ea-php82' => '8.2',
                         'ea-php83' => '8.3',
+                        'ea-php84' => '8.4',
                     ];
 
                     return Arr::get($versions, $this->settings->get('php_system_version'), 'Unknown');
