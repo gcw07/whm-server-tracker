2cf6421255d10932429ac9f5cb1c1db7fa5db31d
Shift <shift@laravelshift.com>
2025-03-15 21:41:23 +0000

    Use Faker methods
    Accessing Faker properties was deprecated in Faker 1.14.

--- database/factories/ServerFactory.php
+++ database/factories/ServerFactory.php
@@ -9,8 +9,8 @@ class ServerFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->domainName,
-            'address' => $this->faker->ipv4,
+            'name' => $this->faker->domainName(),
+            'address' => $this->faker->ipv4(),
             'port' => '2087',
             'server_type' => $this->faker->randomElement(['vps', 'dedicated', 'reseller']),
             'settings' => null,
