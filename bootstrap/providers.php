02c59458fc1091051772367fc4e565faa59b3414
Shift <shift@laravelshift.com>
2025-03-15 21:41:36 +0000

    Re-register service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,8 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Services\WHM\WHMServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\BladeComponentServiceProvider::class,
+    App\Providers\LivewireServiceProvider::class,
 ];
--- config/app.php
+++ config/app.php
@@ -5,24 +5,6 @@ use Illuminate\Support\ServiceProvider;
 
 return [
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Services\WHM\WHMServiceProvider::class,
-        App\Providers\HorizonServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        App\Providers\BladeComponentServiceProvider::class,
-        App\Providers\LivewireServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'Redis' => Illuminate\Support\Facades\Redis::class,
