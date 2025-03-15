5c7c3c33b2fe397fd7afb374b3f5caa19d040885
Shift <shift@laravelshift.com>
2025-03-15 21:41:36 +0000

    Consolidate service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,26 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Encryption\Encrypter;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * The path to your application's "home" route.
+     *
+     * Typically, users are redirected here after authentication.
+     *
+     * @var string
+     */
+    public const HOME = '/dashboard';
+
     /**
      * Register any application services.
      */
@@ -28,6 +41,8 @@ class AppServiceProvider extends ServiceProvider
         $cipher = config('app.cipher');
 
         Model::encryptUsing(new Encrypter($key, $cipher));
+
+        $this->bootRoute();
     }
 
     protected function databaseEncryptionKey(): ?string
@@ -35,5 +50,14 @@ class AppServiceProvider extends ServiceProvider
         $key = config('database.encryption_key');
 
         return base64_decode(Str::after($key, 'base64:'));
+    }
+
+    public function bootRoute(): void
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
+
     }
 }
