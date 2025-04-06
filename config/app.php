4196455223eba2f199d0fb5c853644029e4f4da7
Grant <grant@gwscripts.com>
2025-04-06 02:37:29 -0500

    Add back the timezone env variable and config item.

--- config/app.php
+++ config/app.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Facade;
 
 return [
 
+    'timezone' => env('APP_TIMEZONE', 'UTC'),
+
     'aliases' => Facade::defaultAliases()->merge([
         'Redis' => Illuminate\Support\Facades\Redis::class,
     ])->toArray(),
