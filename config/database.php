fb4404f979b29d75e5fd470e99d022f80db4326b
Grant <grant@gwscripts.com>
2025-08-29 16:36:29 -0500

    Remove v1_db setting in database config file.

--- config/database.php
+++ config/database.php
@@ -2,28 +2,6 @@
 
 return [
 
-    'connections' => [
-        'mysql_v1_db' => [
-            'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('WHM_V1_DB_HOST', '127.0.0.1'),
-            'port' => env('WHM_V1_DB_PORT', '3306'),
-            'database' => env('WHM_V1_DB_DATABASE', 'forge'),
-            'username' => env('WHM_V1_DB_USERNAME', 'forge'),
-            'password' => env('WHM_V1_DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
-    ],
-
     'migrations' => [
         'table' => 'migrations',
         'update_date_on_publish' => false, // disable to preserve original behavior for existing applications
