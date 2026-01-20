92c05f8de23f3b8c14a0c99f8f712f26eb7f05bc
Grant <grant@gwscripts.com>
2026-01-20 00:49:12 -0600

    Remove the alpine tooltip from the app.js.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,9 @@
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import '../../vendor/wire-elements/modal/resources/js/modal';
 
-import Tooltip from "@ryangjchandler/alpine-tooltip";
 import ToastComponent from '../../vendor/usernotnull/tall-toasts/resources/js/tall-toasts';
 import SearchComponent from './search';
 
-Alpine.plugin(Tooltip);
 Alpine.plugin(ToastComponent)
 
 Alpine.data('SearchComponent', SearchComponent);
