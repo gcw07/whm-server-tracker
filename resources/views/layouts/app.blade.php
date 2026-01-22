4c8923593491c7772e02e1a42aec418f02fdb94a
Grant <grant@gwscripts.com>
2026-01-22 15:51:06 -0600

    WIP. Working on converting the header to flux elements.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,37 +17,38 @@
         @fluxAppearance
     </head>
     <body class="min-h-screen bg-gray-100 dark:bg-zinc-800 antialiased">
-        <flux:header container class="bg-cyan-600 dark:bg-zinc-900 border-b border-zinc-400 dark:border-zinc-700">
-          <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
+        <flux:header container class="bg-cyan-600 dark:bg-zinc-900 border-b border-zinc-400 dark:border-zinc-700 h-16">
+          <flux:sidebar.toggle class="lg:hidden text-white!" icon="bars-2" inset="left" />
 
-          <flux:brand href="#" logo="https://fluxui.dev/img/demo/logo.png" name="Acme Inc." class="max-lg:hidden dark:hidden" />
-          <flux:brand href="#" logo="https://fluxui.dev/img/demo/dark-mode-logo.png" name="Acme Inc." class="max-lg:hidden! hidden dark:flex" />
+          <flux:brand href="{{ route('dashboard') }}" class="max-lg:hidden dark:hidden me-8">
+            <x-slot name="logo" class="px-0 h-14!">
+              <img src="/images/logo.svg" alt="{{ config('app.name') }}" class="h-14" />
+            </x-slot>
+          </flux:brand>
+          <flux:brand href="{{ route('dashboard') }}" class="max-lg:hidden! hidden dark:flex">
+            <x-slot name="logo" class="px-0 h-14!">
+              <img src="/images/logo.svg" alt="{{ config('app.name') }}" class="h-14" />
+            </x-slot>
+          </flux:brand>
 
           <flux:navbar class="-mb-px gap-6 max-lg:hidden">
-            <flux:navbar.item icon="server" iconVariant="solid" href="#">Servers</flux:navbar.item>
-            <flux:navbar.item icon="globe-alt" iconVariant="solid" href="#">Accounts</flux:navbar.item>
-            <flux:navbar.item icon="sparkles" iconVariant="solid" href="#">Monitors</flux:navbar.item>
+            <flux:navbar.item icon="server" iconVariant="solid" href="{{ route('servers.index') }}">Servers</flux:navbar.item>
+            <flux:navbar.item icon="globe-alt" iconVariant="solid" href="{{ route('accounts.index') }}">Accounts</flux:navbar.item>
+            <flux:navbar.item icon="sparkles" iconVariant="solid" href="{{ route('monitors.index') }}">Monitors</flux:navbar.item>
           </flux:navbar>
 
           <flux:spacer />
 
-          <flux:navbar class="me-4">
-            <flux:navbar.item icon="magnifying-glass" href="#" label="Search" />
-            <flux:navbar.item class="max-lg:hidden" icon="cog-6-tooth" href="#" label="Settings" />
+          <flux:navbar class="me-8">
+            <flux:input kbd="⌘K" icon="magnifying-glass" placeholder="Search..."/>
           </flux:navbar>
 
-          <flux:dropdown position="top" align="start">
-            <flux:profile avatar="https://fluxui.dev/img/demo/user.png" />
+          <flux:dropdown position="bottom" align="end">
+            <flux:profile name="{{ auth()->user()->name }}" class="hover:bg-cyan-500!" />
 
             <flux:menu>
-              <flux:menu.radio.group>
-                <flux:menu.radio checked>Olivia Martin</flux:menu.radio>
-                <flux:menu.radio>Truly Delta</flux:menu.radio>
-              </flux:menu.radio.group>
-
-              <flux:menu.separator />
-
-              <flux:menu.item icon="arrow-right-start-on-rectangle">Logout</flux:menu.item>
+              <flux:menu.item href="{{ route('users.index') }}" icon="users">Manage Users</flux:menu.item>
+              <flux:menu.item href="{{ route('logout') }}" icon="arrow-right-start-on-rectangle">Sign out</flux:menu.item>
             </flux:menu>
           </flux:dropdown>
         </flux:header>
@@ -65,16 +66,11 @@
           </flux:sidebar.header>
 
           <flux:sidebar.nav>
-            <flux:sidebar.item icon="home" href="#" current>Home</flux:sidebar.item>
-            <flux:sidebar.item icon="inbox" badge="12" href="#">Inbox</flux:sidebar.item>
-            <flux:sidebar.item icon="document-text" href="#">Documents</flux:sidebar.item>
-            <flux:sidebar.item icon="calendar" href="#">Calendar</flux:sidebar.item>
-
-            <flux:sidebar.group expandable heading="Favorites" class="grid">
-              <flux:sidebar.item href="#">Marketing site</flux:sidebar.item>
-              <flux:sidebar.item href="#">Android app</flux:sidebar.item>
-              <flux:sidebar.item href="#">Brand guidelines</flux:sidebar.item>
-            </flux:sidebar.group>
+            <flux:sidebar.item icon="home" href="">Dashboard</flux:sidebar.item>
+            <flux:sidebar.item icon="server" href="">Servers</flux:sidebar.item>
+            <flux:sidebar.item icon="globe-alt" href="">Accounts</flux:sidebar.item>
+            <flux:sidebar.item icon="sparkles" href="">Monitors</flux:sidebar.item>
+            <flux:sidebar.item icon="users" href="">Manage Users</flux:sidebar.item>
           </flux:sidebar.nav>
 
           <flux:sidebar.spacer />
