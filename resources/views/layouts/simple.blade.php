2f180d8a3d311ad6b4b120dc9a4854706abb2ddc
Grant <grant@gwscripts.com>
2026-01-23 17:43:32 -0600

    Formatting.

--- resources/views/layouts/simple.blade.php
+++ resources/views/layouts/simple.blade.php
@@ -20,7 +20,7 @@
       <div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
           {{ $slot }}
       </div>
-      
+
       @livewireScriptConfig
       @fluxScripts
     </body>
