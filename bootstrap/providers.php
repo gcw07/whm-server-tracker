bdf306aa6e726b191478c39ba2d09dc9c9a2d6a9
Grant <grant@gwscripts.com>
2026-01-23 16:00:24 -0600

    Install laravel fortify. This should replace laravel UI that was used previously.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\BladeComponentServiceProvider::class,
+    App\Providers\FortifyServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\LivewireServiceProvider::class,
     App\Services\WHM\WHMServiceProvider::class,
