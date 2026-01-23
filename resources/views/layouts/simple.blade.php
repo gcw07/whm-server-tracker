a3f8144b4e4dbb48f1ecc5dd641612380a6b49f3
Grant <grant@gwscripts.com>
2026-01-23 17:41:54 -0600

    WIP. Get the simple layout working.

--- resources/views/layouts/simple.blade.php
+++ resources/views/layouts/simple.blade.php
@@ -16,21 +16,11 @@
       @livewireStyles
       @fluxAppearance
     </head>
-    <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
-      <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-        <div class="flex w-full max-w-sm flex-col gap-2">
-{{--          <a href="{{ route('dashboard') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>--}}
-{{--                          <span class="flex h-9 w-9 mb-1 items-center justify-center rounded-md">--}}
-{{--                              <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />--}}
-{{--                          </span>--}}
-{{--            <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>--}}
-{{--          </a>--}}
-          <div class="flex flex-col gap-6">
-            {{ $slot }}
-          </div>
-        </div>
+    <body class="min-h-screen bg-gray-50 antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
+      <div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+          {{ $slot }}
       </div>
-
+      
       @livewireScriptConfig
       @fluxScripts
     </body>
