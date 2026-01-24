fd5f2ceb923aa1eee66301442b59c9d1b70f468f
Grant <grant@gwscripts.com>
2026-01-24 14:58:01 -0600

    Remove blade component service provider. It was never used.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\BladeComponentServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\LivewireServiceProvider::class,
