c62af646c6b2ae1a482db600be4005272f007fa9
Grant <grant@gwscripts.com>
2026-01-24 15:00:18 -0600

    Remove Livewire Service Provider. Livewire is auto loaded and you don't need to have one here.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
-    App\Providers\LivewireServiceProvider::class,
     App\Services\WHM\WHMServiceProvider::class,
 ];
