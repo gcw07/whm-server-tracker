db1b62b556451dc23bae0fa1c50e6a03315ce645
Grant <grant@gwscripts.com>
2026-01-25 01:43:06 -0600

    Make the default livewire create command be the multi file type instead of single file.

--- config/livewire.php
+++ config/livewire.php
@@ -69,7 +69,7 @@ return [
     */
 
     'make_command' => [
-        'type' => 'sfc', // Options: 'sfc', 'mfc', 'class'
+        'type' => 'mfc', // Options: 'sfc', 'mfc', 'class'
         'emoji' => true, // Options: true, false
     ],
 
