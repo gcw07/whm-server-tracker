b53c9d0fcb03b23f32ed51841f380e95a6bdee1d
Grant <grant@gwscripts.com>
2026-01-31 22:16:24 -0600

    Add breadcrumbs to the create server page.

--- resources/views/pages/server/⚡create/create.blade.php
+++ resources/views/pages/server/⚡create/create.blade.php
@@ -1,10 +1,16 @@
 <div>
   <!-- Page Header -->
-  <div class="pb-5 border-b border-gray-200 sm:flex sm:items-center sm:justify-between">
-    <h3 class="text-2xl leading-6 font-medium text-gray-900">
-      Servers
-    </h3>
-    <div class="mt-3 sm:mt-0 sm:ml-4">
+  <div class="pb-5 border-b border-gray-200 md:flex md:items-center md:justify-between">
+    <div class="flex-1 min-w-0">
+      <flux:breadcrumbs>
+        <flux:breadcrumbs.item :href="route('dashboard')">Home</flux:breadcrumbs.item>
+        <flux:breadcrumbs.item :href="route('servers.index')">Servers</flux:breadcrumbs.item>
+        <flux:breadcrumbs.item>Create new server</flux:breadcrumbs.item>
+      </flux:breadcrumbs>
+
+      <h3 class="mt-2 text-2xl leading-6 font-medium text-gray-900">
+        Servers
+      </h3>
     </div>
   </div>
   <!-- / End Page Header -->
@@ -14,11 +20,12 @@
 
     <flux:card class="bg-white">
       <flux:heading size="xl">Create new server</flux:heading>
+      <flux:subheading>Enter the required information below to add a new server and start tracking.</flux:subheading>
 
-      <flux:separator />
+      <flux:separator class="my-4" />
 
-      <form wire:submit="save" class="space-y-8 mt-8">
-        <flux:input wire:model="form.name" label="Name" />
+      <form wire:submit="save" class="space-y-8">
+        <flux:input wire:model="form.name" label="Name" required />
 
         <flux:input wire:model="form.address" label="IP Address" />
 
@@ -32,7 +39,7 @@
 
         <flux:textarea wire:model="form.notes" label="Notes" />
 
-        <flux:input wire:model="form.token" label="API Token" description="This is a WHM API Token. Once set this will not be visible again." />
+        <flux:input wire:model="form.token" label="API Token" description:trailing="This is a WHM API Token. Once set this will not be visible again." />
 
         <div class="pt-5 border-t border-gray-200">
           <div class="flex justify-end space-x-3">
