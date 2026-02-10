c65440b3dcdf848c9ce16f12f82311ea99443f9d
Grant <grant@gwscripts.com>
2026-02-10 15:41:39 -0600

    Change the dropdown position on the monitor listings filter dropdown.

--- resources/views/pages/monitor/⚡listings/listings.blade.php
+++ resources/views/pages/monitor/⚡listings/listings.blade.php
@@ -5,7 +5,7 @@
         Monitors
       </h3>
       <div class="mt-3 flex md:mt-0 md:absolute md:top-3 md:right-0 gap-2">
-        <flux:dropdown>
+        <flux:dropdown position="bottom" align="end">
           <flux:button icon="adjustments-horizontal" icon:trailing="chevron-down">
             Filters
             @if($this->filterBy !== 'none')
