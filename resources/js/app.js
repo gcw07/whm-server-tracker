4418e76dd4b78524ad9c21ca6d553668d9849ee7
Grant <grant@gwscripts.com>
2026-02-15 00:28:56 -0600

    Remove the old search javascript. Remove the old navigation component.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,3 @@
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
-import SearchComponent from './search';
-
-Alpine.data('SearchComponent', SearchComponent);
 
 Livewire.start();
