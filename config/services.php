5aa1526129f8b3561397d09b3e958d5c78d4b92d
Grant Williams <grant@gwscripts.com>
2026-04-16 01:34:21 -0500

    Add SES mail setup.

--- config/services.php
+++ config/services.php
@@ -8,6 +8,12 @@ return [
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
     ],
 
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
     'cloudflare' => [
         'api_token' => env('CLOUDFLARE_API_TOKEN'),
     ],
