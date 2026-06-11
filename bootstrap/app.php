13a5df2c8471bf747550c9fbe1137fd9047a71d7
Grant Williams <grant@gwscripts.com>
2026-06-11 01:53:32 -0500

    Keeping but commented out. This might not be needed for anything except Expose tunnelling.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // $middleware->trustProxies(at: '*');
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
